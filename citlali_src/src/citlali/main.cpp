52146f68f9474821ea561bda7c6d7f486f39a378
Jerry Ma <jerry.ma.nk@gmail.com>
2021-01-06 14:33:09 -0500

    udpate yamlconfig

--- citlali_src/src/citlali/main.cpp
+++ citlali_src/src/citlali/main.cpp
@@ -9,15 +9,15 @@
 #include <citlali/gitversion.h>
 #include <cstdlib>
 #include <kids/gitversion.h>
-
-using Config = config::Config;
-using YamlConfig = config::YamlConfig;
+#include <tuple>
 
 auto parse_args(int argc, char *argv[]) {
-    // runtime config container
-    Config rc{};
-    // cli config container
-    Config cc{};
+
+    using FlatConfig = config::Config;
+    // Runtime config container, this is returned.
+    FlatConfig rc{};
+    // CLI config container, this is consumed and discarded.
+    FlatConfig cc{};
     // some of the option specs
     auto citlali_ver = fmt::format("citlali {} ({})", CITLALI_GIT_VERSION,
                                    CITLALI_BUILD_TIMESTAMP);
@@ -54,7 +54,7 @@ auto parse_args(int argc, char *argv[]) {
     // clang-format on
 
     screen.parse(cli, argc, argv);
-    // handle cc
+    // handle CLI config
     SPDLOG_TRACE("cc: {}", cc.pformat());
     if (cc.get_typed<bool>("help")) {
         screen.manpage(cli);
@@ -71,27 +71,42 @@ auto parse_args(int argc, char *argv[]) {
         SPDLOG_DEBUG("reconfigure logger to level={}", log_level_str);
         spdlog::set_level(log_level);
     }
+    // pass on the runtime config
     return rc;
 }
 
-/// @brief The mixin class for validating config
-template <typename Derived> struct ConfigMixin {
-    using config_t = YamlConfig;
+namespace predefs {
+
+/// This namespace contains global settings for common types and constants.
+
+// We need to be careful about the int type used here as we may
+// have very large array in the memory.
+// check eigen index type
+static_assert(std::is_same_v<std::ptrdiff_t, Eigen::Index>,
+              "UNEXPECTED EIGEN INDEX TYPE");
+using index_t = std::ptrdiff_t;
+using shape_t = Eigen::Matrix<index_t, 2, 1>;
+using data_t = double;
+
+} // namespace predefs
+
+/// @brief The mixin class stores and validates config
+template <typename Derived> struct ConfigValidatorMixin {
+    using config_t = config::YamlConfig;
 
 private:
-    using Self = ConfigMixin<Derived>;
-    config_t m_config;
+    config_t m_config{};
     struct derived_has_check_config {
         define_has_member_traits(Derived, check_config);
         constexpr static auto value = has_check_config::value;
     };
 
 public:
-    ConfigMixin() = default;
-    template <typename... Args> ConfigMixin(Args &&...args) {
+    ConfigValidatorMixin() = default;
+    template <typename... Args> ConfigValidatorMixin(Args &&...args) {
         set_config(FWD(args)...);
     }
-    const config_t &config() { return m_config; }
+    const config_t &config() const { return m_config; }
     void set_config(config_t config, bool check = true) {
         if (check) {
             if constexpr (derived_has_check_config::value) {
@@ -110,6 +125,26 @@ public:
             SPDLOG_TRACE("set config without check");
         }
         m_config = std::move(config);
+        SPDLOG_TRACE("m_config:\n{}", m_config.pformat());
+    }
+    static auto from_config(config_t config, bool check = true) {
+        if (check) {
+            if constexpr (derived_has_check_config::value) {
+                if (auto opt_errors = Derived::check_config(config);
+                    opt_errors.has_value()) {
+                    throw std::runtime_error(
+                        fmt::format("invalid config:\n{}\nerrors: {}", config,
+                                    opt_errors.value()));
+                }
+                SPDLOG_TRACE("config check passed");
+            } else {
+                SPDLOG_WARN("config check requested but no "
+                            "check_config found");
+            }
+        } else {
+            SPDLOG_TRACE("config check skipped");
+        }
+        return Derived{std::move(config)};
     }
 };
 
@@ -117,28 +152,18 @@ public:
  * @brief The raw obs struct
  * This represents a single observation that contains a set of data items.
  */
-struct RawObs : ConfigMixin<RawObs> {
+struct RawObs : ConfigValidatorMixin<RawObs> {
     /**
      * @brief The DataItem struct
      * This represent a single data item that belongs to a particular
      * observation
      */
-    struct DataItem : ConfigMixin<DataItem> {
-
-        DataItem(config_t config_)
-            : ConfigMixin<DataItem>{std::move(config_)},
-              interface(config().get_str(std::tuple{"meta", "interface"})),
-              filepath(config().get_str("filepath")) {
-            // initalize io
-            //                 meta::switch_invoke<InterfaceRegistry::InterfaceKind>(
-            //                     [&](auto interfacekind_) {
-            //                         constexpr auto interfacekind =
-            //                             DECAY(interfacekind_)::value;
-            //                         io = InterfaceRegistry::template
-            //                         io_t<interfacekind>();
-            //                     },
-            //                     interface_kind());
-        }
+    struct DataItem : ConfigValidatorMixin<DataItem> {
+        using Base = ConfigValidatorMixin<DataItem>;
+        DataItem(config_t config)
+            : Base{std::move(config)}, interface(this->config().get_str(
+                                           std::tuple{"meta", "interface"})),
+              filepath(this->config().get_str("filepath")) {}
         const std::string interface{};
         const std::string filepath{};
         friend std::ostream &operator<<(std::ostream &os, const DataItem &d) {
@@ -149,7 +174,7 @@ struct RawObs : ConfigMixin<RawObs> {
         static auto check_config(config_t &config)
             -> std::optional<std::string> {
             std::vector<std::string> missing_keys;
-            for (const auto &key : {"interface", "filepath"}) {
+            for (const auto &key : {"meta", "filepath"}) {
                 if (!config.has(key)) {
                     missing_keys.push_back(key);
                 }
@@ -160,18 +185,20 @@ struct RawObs : ConfigMixin<RawObs> {
             return fmt::format("missing keys={}", missing_keys);
         }
     };
-    RawObs(config_t config_)
-        : ConfigMixin<RawObs>{config_}, name{config().get_str("name")} {
+    using Base = ConfigValidatorMixin<RawObs>;
+    RawObs(config_t config)
+        : Base{std::move(config)}, name{this->config().get_str(
+                                       std::tuple{"meta", "name"})} {
+
         // initialize the data_items
-        auto node = config()["data_items"];
-        assert(node.IsSequence());
-        for (std::size_t i = 0; i < node.size(); ++i) {
-            data_items.emplace_back(config_t(node[i]));
+        auto node_data_items = this->config()["data_items"];
+        for (std::size_t i = 0; i < node_data_items.size(); ++i) {
+            data_items.emplace_back(config_t(node_data_items[i]));
         }
     }
     std::string name;
     std::vector<DataItem> data_items{};
-    static auto validate_config(const config_t &config)
+    static auto check_config(const config_t &config)
         -> std::optional<std::string> {
         if (config.has("data_items") && config["data_items"].IsSequence()) {
             return std::nullopt;
@@ -187,6 +214,61 @@ struct RawObs : ConfigMixin<RawObs> {
     }
 };
 
+/**
+ * @brief The Coordinator struct
+ * This wraps around the config object and provides
+ * high level methods in various ways to setup the MPI runtime
+ * with node-local and cross-node environment.
+ */
+struct SeqIOCoordinator : ConfigValidatorMixin<SeqIOCoordinator> {
+    using Base = ConfigValidatorMixin<SeqIOCoordinator>;
+    using index_t = predefs::index_t;
+    using shape_t = predefs::shape_t;
+    using input_t = RawObs;
+    using payload_t = RawObs::DataItem;
+
+    SeqIOCoordinator(config_t config) : Base{std::move(config)} {
+        collect_inputs();
+    }
+
+    // io_buffer
+    using payloads_buffer_data_t = predefs::data_t;
+
+    constexpr auto n_inputs() const { return m_inputs.size(); }
+
+    const std::vector<input_t> &inputs() const { return m_inputs; };
+
+    static auto check_config(const config_t &config)
+        -> std::optional<std::string> {
+        if (config.has("inputs") && config["inputs"].IsSequence()) {
+            return std::nullopt;
+        }
+        return fmt::format("missing key \"inputs\"");
+    }
+    friend std::ostream &operator<<(std::ostream &os,
+                                    const SeqIOCoordinator &co) {
+        return os << fmt::format("SeqIOCoordinator(n_inputs={})",
+                                 co.n_inputs());
+    }
+
+private:
+    std::vector<input_t> m_inputs{}; // stores all the inputs
+
+    // collect input data
+    void collect_inputs() {
+        m_inputs.clear();
+        std::vector<input_t> inputs;
+        auto node_inputs = this->config()["inputs"];
+        assert(node_inputs.IsSequence());
+        for (std::size_t i = 0; i < node_inputs.size(); i++) {
+            inputs.emplace_back(config_t{node_inputs[i]});
+        }
+        m_inputs = std::move(inputs);
+
+        SPDLOG_DEBUG("collected n_inputs={}", n_inputs());
+    }
+};
+
 /// @brief Run citlali reduction.
 /// @param rc The runtime config.
 int run(const config::Config &rc) {
@@ -194,13 +276,17 @@ int run(const config::Config &rc) {
     using kids::KidsDataKind;
     using logging::timeit;
     // IO spec
-    namespace spec = kids::toltec;
-    SPDLOG_INFO("use data spec: {}", spec::name);
+    namespace kidsdata = kids::toltec;
+    SPDLOG_INFO("use KIDs data spec: {}", kidsdata::name);
 
-    // load the config
-    auto config_filepath = rc.get_str("config_file");
-    auto config = YamlConfig(YAML::LoadFile(config_filepath));
-    SPDLOG_TRACE("citlali config:\n{}", config.pformat());
+    // load the yaml citlali config
+    auto citlali_config =
+        config::YamlConfig::from_filepath(rc.get_str("config_file"));
+    SPDLOG_TRACE("citlali config:\n{}", citlali_config.pformat());
+
+    // set up the IO coorindator
+    auto co = SeqIOCoordinator::from_config(citlali_config);
+    SPDLOG_TRACE("pipeline coordinator: {}", co);
     return EXIT_SUCCESS;
 }
 
