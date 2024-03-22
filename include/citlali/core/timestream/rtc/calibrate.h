ea07ed7d0361489e1b0548978bdf2f7d52a71b77
mmccrackan <mmccrackan@umass.edu>
2024-03-22 15:06:37 -0400

    test update for atm model

--- include/citlali/core/timestream/rtc/calibrate.h
+++ include/citlali/core/timestream/rtc/calibrate.h
@@ -56,41 +56,51 @@ public:
         for (const auto &[key,val]: tx_225_zenith) {
             if (abs(tau_225_zenith - tau_225_calc(i)) < tau_225_diff) {
                 extinction_model = key;
+                tau_225_diff = abs(tau_225_zenith - tau_225_calc(i));
             }
             i++;
         }
 
-        // allocate transmission coefficients (order 4 polynomial)
-        tx_ratio_coeff["a1100"].resize(5);
-        tx_ratio_coeff["a1400"].resize(5);
-        tx_ratio_coeff["a2000"].resize(5);
+        // allocate transmission coefficients (order 6 polynomial)
+        tx_ratio_coeff["a1100"].resize(7);
+        tx_ratio_coeff["a1400"].resize(7);
+        tx_ratio_coeff["a2000"].resize(7);
 
         // am_q25
         if (extinction_model=="am_q25") {
-            tx_ratio_coeff["a1100"] << -0.07645234, 0.32631179, -0.53526165,  0.41427551,  0.8445016;
-            tx_ratio_coeff["a1400"] << 0.01602193, -0.06815535,  0.11116415, -0.08525746,  1.03135867;
-            tx_ratio_coeff["a2000"] << 0.09942805, -0.42193151,  0.68537969, -0.52220652,  1.18925417;
+            tx_ratio_coeff["a1100"] << -0.12008024,  0.72422015, -1.81734478,  2.45313012, -1.92159695,  0.86918801, 0.78604295;
+            tx_ratio_coeff["a1400"] << 0.02619509, -0.15757661, 0.39400473, -0.52912696, 0.411213, -0.18360141, 1.04398466;
+            tx_ratio_coeff["a2000"] << 0.16726241, -1.00436302,  2.50507317, -3.35219659, 2.59080373, -1.14622096, 1.26931683;
         }
 
         // am_q50
         else if (extinction_model=="am_q50") {
-            tx_ratio_coeff["a1100"] << -0.12248821,  0.52388237, -0.86235963,  0.67117711,  0.74494326;
-            tx_ratio_coeff["a1400"] << 0.02615391, -0.11122017,  0.18130761, -0.13893473,  1.05100321;
-            tx_ratio_coeff["a2000"] << 0.20312343, -0.85983102,  1.39084759, -1.05255297,  1.3756457;
+            tx_ratio_coeff["a1100"] << -0.18770822,  1.13390437, -2.85173457,  3.8617083,  -3.03996805,  1.38624303,
+                                           0.65300169;
+            tx_ratio_coeff["a1400"] << 0.04292884, -0.25817762,  0.64533115, -0.86622214,  0.67267823, -0.29996916,
+                                           1.07167603;
+            tx_ratio_coeff["a2000"] << 0.35178447, -2.10859714,  5.24620825, -6.9952531,   5.37645792, -2.35675076,
+                                           1.54286813;
         }
 
         // am_q75
         else if (extinction_model=="am_q75") {
-            tx_ratio_coeff["a1100"] << -0.19311728,  0.82931153, -1.37452105,  1.08148198,  0.57913582;
-            tx_ratio_coeff["a1400"] << 0.04588952, -0.19502626,  0.31759789, -0.24297199,  1.08886782;
-            tx_ratio_coeff["a2000"] << 0.43837991, -1.84767188,  2.96708103, -2.21900446,  1.77085296;
+            tx_ratio_coeff["a1100"] << -0.28189529,  1.70842347, -4.31606883,  5.88248549, -4.67702093,  2.16747228,
+                                           0.4393435;
+            tx_ratio_coeff["a1400"] << 0.07581154, -0.45574885,  1.13852458, -1.52697451,  1.18425865, -0.5269455,
+                                           1.12531753;
+            tx_ratio_coeff["a2000"] << 0.79869908,  -4.77265095, 11.82393401, -15.67007557,  11.93052031,
+                                           -5.14788907,   2.14595898;
         }
 
         // am_q95
         else if (extinction_model=="am_q95") {
-            tx_ratio_coeff["a1100"] << -0.6973269, 2.7011668, -3.93198542, 2.65377595, 0.129337;
-            tx_ratio_coeff["a1400"] << 0.2933151, -1.10126494, 1.52262009, -0.9424269, 1.25622249;
-            tx_ratio_coeff["a2000"] << 4.87342355, -17.94456945,  24.02708919, -14.06116984, 4.35136574;
+            tx_ratio_coeff["a1100"] <<  -1.21882233,   6.67068453, -14.96466875,  17.78045563, -12.10288687,
+                                           4.76050807,  -0.06765066;
+            tx_ratio_coeff["a1400"] << 0.76090502, -4.05867663,  8.78487281, -9.90872343,  6.2198602,  -2.13790165,
+                                           1.3668983;
+            tx_ratio_coeff["a2000"] << 16.0063036,   -84.30325144, 179.28096414, -197.05751682, 118.73627425,
+                                           -37.99279818, 6.55457576;
         }
     }
 
@@ -99,9 +109,15 @@ public:
     auto tau_polynomial(Eigen::DenseBase<DerivedA> &coeff, Eigen::DenseBase<DerivedB> &elev) {
 
         // p(x) = a4*x^4 + a3*x^3 + a2*x^2 + a1*x + a0
-        return coeff(0)*pow(elev.derived().array(),4) + coeff(1)*pow(elev.derived().array(),3) +
+        /*return coeff(0)*pow(elev.derived().array(),4) + coeff(1)*pow(elev.derived().array(),3) +
                coeff(2)*pow(elev.derived().array(),2) + coeff(3)*pow(elev.derived().array(),1) +
-               coeff(4);
+               coeff(4);*/
+
+        // p(x) = a6*x^6 + a5*x^5 + a4*x^4 + a3*x^3 + a2*x^2 + a1*x + a0
+        return coeff(0)*pow(elev.derived().array(),6) + coeff(1)*pow(elev.derived().array(),5) +
+               coeff(2)*pow(elev.derived().array(),4) + coeff(3)*pow(elev.derived().array(),3) +
+               coeff(4)*pow(elev.derived().array(),2) + coeff(5)*pow(elev.derived().array(),1) +
+               coeff(6);
     }
 
     // calculate tau for each toltec band for given elevations
