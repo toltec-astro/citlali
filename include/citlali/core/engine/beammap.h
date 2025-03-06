175fb84048daf79192872ff8b11fa19d41745eb7
mmccrackan <mmccrackan@umass.edu>
2025-03-05 23:54:25 -0500

    faster jinc mapmaking when beammapping

--- include/citlali/core/engine/beammap.h
+++ include/citlali/core/engine/beammap.h
@@ -634,6 +634,7 @@ void Beammap::run_loop() {
         tula::logging::progressbar pb(
             [&](const auto &msg) { logger->info("{}", msg); }, 100, "PTC progress ");
 
+
         // cleaning (separate from mapmaking loop due to jinc mapmaking parallelization)
         grppi::map(tula::grppi_utils::dyn_ex(omb.parallel_policy), scan_in_vec, scan_out_vec, [&](auto i) {
             if (run_mapmaking) {
@@ -755,26 +756,31 @@ void Beammap::run_loop() {
 
             logger->info("running mapmaking");
 
-            // mapmaking
-            grppi::map(tula::grppi_utils::dyn_ex(map_parallel_policy), scan_in_vec, scan_out_vec, [&](auto i) {
+            if (map_method == "jinc") {
                 bool run_omb = true;
-                // populate maps
-                if (map_method=="naive") {
+                for (auto& ptc : ptcs) {
+                    // jinc mapmaker
+                    jinc_mm.populate_maps_jinc_parallel(ptc, omb, cmb, ptc.map_indices.data, telescope.pixel_axes,
+                                                        calib.apt, telescope.d_fsmp, run_omb, run_noise);
+                    // update progress bar
+                    pb.count(telescope.scan_indices.cols(), 1);
+                }
+            }
+
+            else if (map_method == "naive") {
+                // mapmaking
+                grppi::map(tula::grppi_utils::dyn_ex(map_parallel_policy), scan_in_vec, scan_out_vec, [&](auto i) {
+                    bool run_omb = true;
                     // naive mapmaker
                     naive_mm.populate_maps_naive(ptcs[i], omb, cmb, ptcs[i].map_indices.data, telescope.pixel_axes,
-                                                 calib.apt, telescope.d_fsmp, run_omb, run_noise);
-                }
-                else if (map_method=="jinc") {
-                    // jinc mapmaker
-                    jinc_mm.populate_maps_jinc(ptcs[i], omb, cmb, ptcs[i].map_indices.data, telescope.pixel_axes,
-                                               calib.apt, telescope.d_fsmp, run_omb, run_noise);
-                }
+                                                calib.apt, telescope.d_fsmp, run_omb, run_noise);
 
-                // update progress bar
-                pb.count(telescope.scan_indices.cols(), 1);
+                    // update progress bar
+                    pb.count(telescope.scan_indices.cols(), 1);
 
-                return 0;
-            });
+                    return 0;
+                });
+        }
 
             // normalize maps
             logger->info("normalizing maps");
@@ -932,20 +938,27 @@ void Beammap::set_apt_flags() {
         return 0;
     });
 
+
     // median network sensitivity for flagging
     std::map<Eigen::Index, double> nw_median_sens;
 
     // calc median sens from unflagged detectors for each nw
     logger->debug("calculating mean sensitivities");
+    logger->debug("calib.n_nws {}", calib.n_nws);
     for (Eigen::Index i=0; i<calib.n_nws; ++i) {
         Eigen::Index nw = calib.apt["nw"](i);
 
         // nw sensitivity
         auto nw_sens = calib.apt["sens"](Eigen::seq(std::get<0>(calib.nw_limits[nw]),
                                                     std::get<1>(calib.nw_limits[nw])-1));
+
+        logger->debug("nw_sens {}", nw_sens);
+
         // number of good detectors
         Eigen::Index n_good_det = (calib.apt["flag"](Eigen::seq(std::get<0>(calib.nw_limits[nw]),
                                                                std::get<1>(calib.nw_limits[nw])-1)).array()==0).count();
+
+       logger->debug("n_good_det {}", n_good_det);
 
         if (n_good_det>0) {
             // to hold good detectors
@@ -953,6 +966,7 @@ void Beammap::set_apt_flags() {
 
             // remove flagged dets
             Eigen::Index j = std::get<0>(calib.nw_limits[nw]);
+            logger->debug("j {}", j);
             Eigen::Index k = 0;
             for (Eigen::Index m=0; m<sens.size(); m++) {
                 if (calib.apt["flag"](j)==0) {
@@ -961,13 +975,16 @@ void Beammap::set_apt_flags() {
                 }
                 j++;
             }
+            logger->debug("sens {}", sens);
             // calculate median sens
             nw_median_sens[nw] = tula::alg::median(sens);
         }
         else {
             nw_median_sens[nw] = tula::alg::median(nw_sens);
         }
+        logger->debug("mean sens {}", nw_median_sens[nw]);
     }
+
 
     // flag too low/high sensitivies based on the median unflagged sensitivity of each nw
     logger->debug("flagging sensitivities");
