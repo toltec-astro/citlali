0b88648f0e7c4089e79edcbe1181c8af79a767e1
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 14:01:27 -0400

    extract observation output dirs helper

---
+++ include/citlali/core/pipeline/observation_output_dirs.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <filesystem>
+
+namespace citlali::pipeline {
+
+template <class Engine, class Logger>
+void create_observation_output_dirs(const Engine &engine,
+                                    const Logger &logger) {
+    namespace fs = std::filesystem;
+
+    logger->debug("creating obsnum directory");
+    fs::create_directories(engine.obsnum_dir_name);
+
+    logger->debug("creating obsnum raw directory");
+    fs::create_directories(engine.obsnum_dir_name + "raw/");
+
+    if (!engine.run_coadd) {
+        if (engine.run_map_filter) {
+            logger->debug("creating obsnum filtered directory");
+            fs::create_directories(engine.obsnum_dir_name + "filtered/");
+        }
+    }
+    if (engine.verbose_mode) {
+        logger->debug("creating obsnum logs directory");
+        fs::create_directories(engine.obsnum_dir_name + "logs/");
+    }
+}
+
+}  // namespace citlali::pipeline
--- include/citlali/core/pipeline/observation_output_layout.h
+++ include/citlali/core/pipeline/observation_output_layout.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <citlali/core/pipeline/obsnum_format.h>
+#include <citlali/core/pipeline/observation_output_dirs.h>
 
-#include <filesystem>
 
 namespace citlali::pipeline {
 
@@ -16,29 +16,6 @@ void configure_observation_output_layout(Engine &engine, int obsnum) {
 
     if (engine.run_coadd) {
         engine.cmb.obsnums.push_back(engine.obsnum);
-    }
-}
-
-template <class Engine, class Logger>
-void create_observation_output_dirs(const Engine &engine,
-                                    const Logger &logger) {
-    namespace fs = std::filesystem;
-
-    logger->debug("creating obsnum directory");
-    fs::create_directories(engine.obsnum_dir_name);
-
-    logger->debug("creating obsnum raw directory");
-    fs::create_directories(engine.obsnum_dir_name + "raw/");
-
-    if (!engine.run_coadd) {
-        if (engine.run_map_filter) {
-            logger->debug("creating obsnum filtered directory");
-            fs::create_directories(engine.obsnum_dir_name + "filtered/");
-        }
-    }
-    if (engine.verbose_mode) {
-        logger->debug("creating obsnum logs directory");
-        fs::create_directories(engine.obsnum_dir_name + "logs/");
     }
 }
 
