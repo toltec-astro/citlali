a279b26d65aa4a8a92da66650d60b3c2fcb1fff8
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 12:15:40 -0400

    extract output config copy helpers

--- include/citlali/core/pipeline/output_layout.h
+++ include/citlali/core/pipeline/output_layout.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <citlali/core/pipeline/output_config_copy.h>
+
 #include <filesystem>
 #include <fstream>
 #include <iomanip>
@@ -8,32 +10,6 @@
 #include <vector>
 
 namespace citlali::pipeline {
-
-inline std::string config_copy_filename(const std::string &config_filepath) {
-    const auto last_slash_pos = config_filepath.find_last_of("/");
-    if (last_slash_pos == std::string::npos) {
-        return config_filepath;
-    }
-    return config_filepath.substr(last_slash_pos + 1);
-}
-
-inline std::string config_copy_destination(const std::string &reduction_dir,
-                                           const std::string &config_filepath) {
-    return reduction_dir + "/" + config_copy_filename(config_filepath);
-}
-
-template <class Logger>
-void copy_config_files_to_reduction_dir(
-    const std::vector<std::string> &config_filepaths,
-    const std::string &reduction_dir, const Logger &logger) {
-    namespace fs = std::filesystem;
-    for (const auto &config_filepath : config_filepaths) {
-        logger->debug("copying config files into redu directory");
-        fs::copy(config_filepath,
-                 config_copy_destination(reduction_dir, config_filepath),
-                 fs::copy_options::overwrite_existing);
-    }
-}
 
 template <class TodProc, class ConfigFilepaths, class Logger>
 void prepare_iteration_output_layout_if_needed(
