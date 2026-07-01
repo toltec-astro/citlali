a64922f0d252ca99cbcd0de29bda58f187619f13
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 14:03:36 -0400

    use direct rawobs output layout include

--- include/citlali/core/pipeline/output_layout.h
+++ include/citlali/core/pipeline/output_layout.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #include <citlali/core/pipeline/iteration_output_layout.h>
-#include <citlali/core/pipeline/observation_output_layout.h>
 #include <citlali/core/pipeline/output_config_copy.h>
+#include <citlali/core/pipeline/rawobs_observation_output_layout.h>
 #include <citlali/core/pipeline/timing_gap_output.h>
