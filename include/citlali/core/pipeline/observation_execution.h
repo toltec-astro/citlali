96f5aa0f8b94468436ca6bbc4bf4c746b85f0f76
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 11:41:25 -0400

    extract reduction iteration runner

--- include/citlali/core/pipeline/observation_execution.h
+++ include/citlali/core/pipeline/observation_execution.h
@@ -9,6 +9,7 @@
 #include <citlali/core/pipeline/observation_outputs.h>
 #include <citlali/core/pipeline/observation_preflight.h>
 #include <citlali/core/pipeline/observation_pipeline.h>
+#include <citlali/core/pipeline/reduction_iteration.h>
 #include <citlali/core/pipeline/reduction_iteration_outputs.h>
 #include <citlali/core/pipeline/reduction_observation.h>
 #include <citlali/core/pipeline/reduction_observation_inputs.h>
@@ -22,30 +23,6 @@
 #include <utility>
 
 namespace citlali::pipeline {
-
-template <bool IsBeammap, auto RawObsMap, auto FilteredObsMap,
-          auto RawCoaddMap, auto FilteredCoaddMap, bool FitMaps,
-          class KidsDataProc, class TodProc, class IOCoordinator,
-          class CitlaliConfig, class ConfigFilepaths, class MapExtents,
-          class MapCoords, class DateObsFactory, class Logger>
-bool run_reduction_iteration(
-    TodProc &todproc, const IOCoordinator &co, CitlaliConfig &citlali_config,
-    const ConfigFilepaths &config_filepaths, MapExtents &map_extents,
-    MapCoords &map_coords, DateObsFactory &&date_obs_factory,
-    const Logger &logger) {
-    begin_reduction_iteration(todproc, config_filepaths, logger);
-
-    if (!run_reduction_iteration_observations<
-            IsBeammap, RawObsMap, FilteredObsMap, FitMaps, KidsDataProc>(
-            todproc, co, citlali_config, map_extents, map_coords,
-            date_obs_factory, logger)) {
-        return false;
-    }
-
-    finish_reduction_iteration<RawCoaddMap, FilteredCoaddMap>(todproc,
-                                                              logger);
-    return true;
-}
 
 template <bool IsBeammap, auto RawObsMap, auto FilteredObsMap,
           auto RawCoaddMap, auto FilteredCoaddMap, bool FitMaps,
