51b327614bdecb9bd49089fbbf496ee789ec9090
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 13:43:26 -0400

    use direct preflight telescope includes

--- include/citlali/core/pipeline/observation_preflight.h
+++ include/citlali/core/pipeline/observation_preflight.h
@@ -8,8 +8,10 @@
 #include <citlali/core/pipeline/observation_detector_diagnostics.h>
 #include <citlali/core/pipeline/observation_input_checks.h>
 #include <citlali/core/pipeline/observation_sample_rate.h>
-#include <citlali/core/pipeline/observation_telescope.h>
 #include <citlali/core/pipeline/observation_timing.h>
 #include <citlali/core/pipeline/rawobs_data_items.h>
 #include <citlali/core/pipeline/reduction_observation_calibration.h>
 #include <citlali/core/pipeline/reduction_config.h>
+#include <citlali/core/pipeline/scan_indices.h>
+#include <citlali/core/pipeline/telescope_data_loading.h>
+#include <citlali/core/pipeline/telescope_pointing.h>
