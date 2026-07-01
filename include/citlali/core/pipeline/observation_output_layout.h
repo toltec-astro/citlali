0acd11cb3233fa505b9715c78e1884b7ad0489ff
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 14:03:15 -0400

    make observation output layout a facade

--- include/citlali/core/pipeline/observation_output_layout.h
+++ include/citlali/core/pipeline/observation_output_layout.h
@@ -1,6 +1,3 @@
 #pragma once
 
-#include <citlali/core/pipeline/obsnum_format.h>
-#include <citlali/core/pipeline/observation_output_config.h>
-#include <citlali/core/pipeline/observation_output_dirs.h>
 #include <citlali/core/pipeline/rawobs_observation_output_layout.h>
