3230e5a57a37d7c947c71deaf08efe901629eaba
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 15:23:20 -0400

    extract raw observation output helpers

--- include/citlali/core/pipeline/observation_outputs.h
+++ include/citlali/core/pipeline/observation_outputs.h
@@ -1,40 +1,9 @@
 #pragma once
 
 #include <citlali/core/pipeline/output_policy.h>
+#include <citlali/core/pipeline/raw_observation_outputs.h>
 
 namespace citlali::pipeline {
-
-template <class Engine, class Logger>
-void calculate_raw_observation_noise_products_if_needed(
-    Engine &engine, const Logger &logger) {
-    if (engine.run_mapmaking &&
-        engine.run_noise_products &&
-        engine.run_noise) {
-        logger->info("calculating raw obs empirical noise products");
-        engine.omb.calc_noise_products(engine.apply_empirical_noise_weights);
-    }
-}
-
-template <auto RawObsMap, class Engine, class Logger>
-void output_raw_observation_maps_if_needed(Engine &engine,
-                                           const Logger &logger) {
-    if (engine.run_mapmaking) {
-        engine.create_obs_map_files();
-        logger->info("outputting raw obs files");
-        engine.template output<RawObsMap>();
-    }
-    else {
-        logger->info("mapmaking disabled; skipping raw obs map output");
-    }
-}
-
-template <auto RawObsMap, class TodProc, class Logger>
-void write_raw_observation_outputs(TodProc &todproc, const Logger &logger) {
-    auto &engine = todproc.engine();
-
-    calculate_raw_observation_noise_products_if_needed(engine, logger);
-    output_raw_observation_maps_if_needed<RawObsMap>(engine, logger);
-}
 
 template <auto FilteredObsMap, class Engine, class Logger>
 void filter_observation_maps(Engine &engine, const Logger &logger) {
---
+++ include/citlali/core/pipeline/raw_observation_outputs.h
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace citlali::pipeline {
+
+template <class Engine, class Logger>
+void calculate_raw_observation_noise_products_if_needed(
+    Engine &engine, const Logger &logger) {
+    if (engine.run_mapmaking &&
+        engine.run_noise_products &&
+        engine.run_noise) {
+        logger->info("calculating raw obs empirical noise products");
+        engine.omb.calc_noise_products(engine.apply_empirical_noise_weights);
+    }
+}
+
+template <auto RawObsMap, class Engine, class Logger>
+void output_raw_observation_maps_if_needed(Engine &engine,
+                                           const Logger &logger) {
+    if (engine.run_mapmaking) {
+        engine.create_obs_map_files();
+        logger->info("outputting raw obs files");
+        engine.template output<RawObsMap>();
+    }
+    else {
+        logger->info("mapmaking disabled; skipping raw obs map output");
+    }
+}
+
+template <auto RawObsMap, class TodProc, class Logger>
+void write_raw_observation_outputs(TodProc &todproc, const Logger &logger) {
+    auto &engine = todproc.engine();
+
+    calculate_raw_observation_noise_products_if_needed(engine, logger);
+    output_raw_observation_maps_if_needed<RawObsMap>(engine, logger);
+}
+
+}  // namespace citlali::pipeline
