a5f1dafde67935dda2be7af089fc1e40407595da
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 13:00:27 -0400

    extract hwpr loading helper

---
+++ include/citlali/core/pipeline/hwpr_loading.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+
+namespace citlali::pipeline {
+
+template <class Engine, class RawObs, class Logger>
+void load_hwpr_data_if_requested(Engine &engine, const RawObs &rawobs,
+                                 const Logger &logger) {
+    if (engine.rtcproc.run_polarization) {
+        std::string hwpr_filepath;
+        if (rawobs.hwpdata().has_value() && engine.calib.ignore_hwpr != "true") {
+            hwpr_filepath = rawobs.hwpdata()->filepath();
+            if (hwpr_filepath != "null") {
+                logger->info("getting hwpr file {}", hwpr_filepath);
+                engine.calib.get_hwpr(hwpr_filepath, engine.telescope.sim_obs);
+            }
+            else {
+                engine.calib.run_hwpr = false;
+            }
+        }
+        else {
+            engine.calib.run_hwpr = false;
+        }
+        if (!engine.calib.run_hwpr) {
+            logger->info("ignoring hwpr");
+        }
+    }
+}
+
+}  // namespace citlali::pipeline
--- include/citlali/core/pipeline/observation_calibration.h
+++ include/citlali/core/pipeline/observation_calibration.h
@@ -1,37 +1,6 @@
 #pragma once
 
 #include <citlali/core/pipeline/flux_calibration.h>
+#include <citlali/core/pipeline/hwpr_loading.h>
 #include <citlali/core/pipeline/observation_calibration_config.h>
 #include <citlali/core/pipeline/reduction_observation_calibration.h>
-
-#include <cmath>
-#include <string>
-#include <vector>
-
-namespace citlali::pipeline {
-
-template <class Engine, class RawObs, class Logger>
-void load_hwpr_data_if_requested(Engine &engine, const RawObs &rawobs,
-                                 const Logger &logger) {
-    if (engine.rtcproc.run_polarization) {
-        std::string hwpr_filepath;
-        if (rawobs.hwpdata().has_value() && engine.calib.ignore_hwpr != "true") {
-            hwpr_filepath = rawobs.hwpdata()->filepath();
-            if (hwpr_filepath != "null") {
-                logger->info("getting hwpr file {}", hwpr_filepath);
-                engine.calib.get_hwpr(hwpr_filepath, engine.telescope.sim_obs);
-            }
-            else {
-                engine.calib.run_hwpr = false;
-            }
-        }
-        else {
-            engine.calib.run_hwpr = false;
-        }
-        if (!engine.calib.run_hwpr) {
-            logger->info("ignoring hwpr");
-        }
-    }
-}
-
-}  // namespace citlali::pipeline
