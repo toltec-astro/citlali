9f925c15a8a1d9c496e3309ad148122b30cd4136
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 12:18:40 -0400

    extract flxscale correction helper

--- include/citlali/core/pipeline/observation_calibration.h
+++ include/citlali/core/pipeline/observation_calibration.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <citlali/core/pipeline/flxscale_correction.h>
 #include <citlali/core/pipeline/kids_metadata.h>
 #include <citlali/core/pipeline/rawobs_data_items.h>
 
@@ -8,36 +9,6 @@
 #include <vector>
 
 namespace citlali::pipeline {
-
-template <class Engine, class RawObs, class Logger>
-bool apply_flxscale_correction(Engine &engine, const RawObs &rawobs,
-                               const Logger &logger) {
-    const auto *flxscale_corr = rawobs.flxscale_correction();
-    if (flxscale_corr == nullptr) {
-        return true;
-    }
-
-    const double factor = flxscale_corr->value();
-    if (!std::isfinite(factor) || factor <= 0.0) {
-        logger->error(
-            "invalid flxscale_correction={} for observation {}; "
-            "factor must be finite and > 0",
-            factor, rawobs.name());
-        return false;
-    }
-    if (engine.calib.apt.count("flxscale") == 0) {
-        logger->error(
-            "flxscale column missing from APT while applying "
-            "flxscale_correction for observation {}",
-            rawobs.name());
-        return false;
-    }
-
-    engine.calib.apt["flxscale"].array() *= factor;
-    logger->info("applied flxscale correction factor={} for observation {}",
-                 factor, rawobs.name());
-    return true;
-}
 
 template <class Engine, class RawObs, class Logger>
 void load_array_properties_table(Engine &engine, const RawObs &rawobs,
