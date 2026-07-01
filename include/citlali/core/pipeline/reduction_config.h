23948c8d09416a4d8f97a7a3299415e165ee469e
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 11:43:14 -0400

    extract reduction config helpers

---
+++ include/citlali/core/pipeline/reduction_config.h
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace citlali::pipeline {
+
+template <class Engine>
+bool engine_config_has_errors(const Engine &engine) {
+    return !engine.missing_keys.empty() || !engine.invalid_keys.empty();
+}
+
+template <class Engine, class Config, class Logger>
+bool load_and_validate_engine_config(Engine &engine, Config &config,
+                                     const Logger &logger) {
+    logger->info("getting citlali config");
+    engine.get_citlali_config(config);
+
+    if (!engine_config_has_errors(engine)) {
+        return true;
+    }
+
+    logger->error("missing or invalid keys were found!");
+    logger->error(
+        "see for default config: "
+        "https://github.com/toltec-astro/citlali/blob/v4.x/data/config.yaml");
+    return false;
+}
+
+template <class Engine, class Logger, class EnableDebugLogging>
+void configure_verbose_logging_if_requested(
+    const Engine &engine, const Logger &logger,
+    EnableDebugLogging &&enable_debug_logging) {
+    if (!engine.verbose_mode) {
+        return;
+    }
+
+    enable_debug_logging();
+    logger->debug("running in verbose mode. setting log level=debug.");
+}
+
+}  // namespace citlali::pipeline
