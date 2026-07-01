3eaef42d1201f8310d4e2f47d230858d16326fe4
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 12:00:43 -0400

    use direct observation loop includes

--- include/citlali/core/pipeline/reduction_observation_loop.h
+++ include/citlali/core/pipeline/reduction_observation_loop.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <citlali/core/pipeline/observation_preflight.h>
+#include <citlali/core/pipeline/kids_metadata.h>
 #include <citlali/core/pipeline/reduction_observation.h>
 
 #include <cstddef>
