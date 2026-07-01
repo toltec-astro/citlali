4998a0e5d6ddd5ccab782f40cf22767bb1b6184d
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 11:59:00 -0400

    extract observation sample rate helpers

--- include/citlali/core/pipeline/observation_preflight.h
+++ include/citlali/core/pipeline/observation_preflight.h
@@ -3,6 +3,7 @@
 #include <citlali/core/pipeline/kids_metadata.h>
 #include <citlali/core/pipeline/observation_calibration.h>
 #include <citlali/core/pipeline/observation_input_checks.h>
+#include <citlali/core/pipeline/observation_sample_rate.h>
 #include <citlali/core/pipeline/observation_telescope.h>
 #include <citlali/core/pipeline/observation_timing.h>
 #include <citlali/core/pipeline/rawobs_data_items.h>
@@ -14,56 +15,6 @@
 #include <vector>
 
 namespace citlali::pipeline {
-
-template <class Engine, class Logger>
-bool configure_effective_sample_rate(Engine &engine, const Logger &logger) {
-    if (engine.rtcproc.run_downsample) {
-        if (engine.rtcproc.downsampler.factor <= 0) {
-            if (engine.rtcproc.downsampler.downsampled_freq_Hz <= 0) {
-                logger->error(
-                    "downsampled freq ({} Hz) must be > 0 when downsample "
-                    "factor <= 0",
-                    engine.rtcproc.downsampler.downsampled_freq_Hz);
-                return false;
-            }
-            if (engine.rtcproc.downsampler.downsampled_freq_Hz >
-                engine.telescope.fsmp) {
-                logger->error(
-                    "downsampled freq ({} Hz) must be less than sample rate "
-                    "({} Hz)",
-                    engine.rtcproc.downsampler.downsampled_freq_Hz,
-                    engine.telescope.fsmp);
-                return false;
-            }
-            engine.rtcproc.downsampler.factor = std::floor(
-                engine.telescope.fsmp /
-                engine.rtcproc.downsampler.downsampled_freq_Hz);
-        }
-        if (engine.rtcproc.downsampler.factor <= 0) {
-            logger->error("downsample factor ({}) must be > 0",
-                          engine.rtcproc.downsampler.factor);
-            return false;
-        }
-
-        const double downsample_nyquist_Hz =
-            engine.telescope.fsmp /
-            (2.0 * engine.rtcproc.downsampler.factor);
-        if (engine.rtcproc.filter.freq_high_Hz > downsample_nyquist_Hz) {
-            logger->error(
-                "invalid anti-alias setup: filter freq_high_Hz ({} Hz) "
-                "exceeds downsample Nyquist ({} Hz)",
-                engine.rtcproc.filter.freq_high_Hz,
-                downsample_nyquist_Hz);
-            return false;
-        }
-        engine.telescope.d_fsmp =
-            engine.telescope.fsmp / engine.rtcproc.downsampler.factor;
-    }
-    else {
-        engine.telescope.d_fsmp = engine.telescope.fsmp;
-    }
-    return true;
-}
 
 template <class TodProc, class RawObs, class Logger>
 void load_raw_detector_diagnostics(TodProc &todproc, const RawObs &rawobs,
