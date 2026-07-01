08c970d981274dbe1bef8ad54d93b95809d15518
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 15:27:54 -0400

    make coadd outputs a facade

--- include/citlali/core/pipeline/coadd_outputs.h
+++ include/citlali/core/pipeline/coadd_outputs.h
@@ -1,6 +1,3 @@
 #pragma once
 
-#include <citlali/core/pipeline/filtered_coadd_outputs.h>
 #include <citlali/core/pipeline/iteration_coadd_outputs.h>
-#include <citlali/core/pipeline/output_policy.h>
-#include <citlali/core/pipeline/raw_coadd_outputs.h>
