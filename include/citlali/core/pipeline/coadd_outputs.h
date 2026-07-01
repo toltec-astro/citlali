405dcff330d49b688bfa456678d473d893509b97
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 15:27:38 -0400

    extract iteration coadd output helper

--- include/citlali/core/pipeline/coadd_outputs.h
+++ include/citlali/core/pipeline/coadd_outputs.h
@@ -1,23 +1,6 @@
 #pragma once
 
 #include <citlali/core/pipeline/filtered_coadd_outputs.h>
+#include <citlali/core/pipeline/iteration_coadd_outputs.h>
 #include <citlali/core/pipeline/output_policy.h>
 #include <citlali/core/pipeline/raw_coadd_outputs.h>
-
-namespace citlali::pipeline {
-
-template <auto RawCoaddMap, auto FilteredCoaddMap, class TodProc,
-          class Logger>
-void write_iteration_coadd_outputs_if_needed(TodProc &todproc,
-                                             const Logger &logger) {
-    auto &engine = todproc.engine();
-
-    if (!should_write_iteration_coadd_outputs(engine)) {
-        return;
-    }
-
-    write_raw_coadd_outputs<RawCoaddMap>(todproc, logger);
-    write_filtered_coadd_outputs_if_needed<FilteredCoaddMap>(todproc, logger);
-}
-
-}  // namespace citlali::pipeline
