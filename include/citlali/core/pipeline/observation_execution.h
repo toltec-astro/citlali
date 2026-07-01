9d70786b4325e497d41d0bd346b6edd65ade9a24
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 11:29:06 -0400

    extract reduction observation runner

--- include/citlali/core/pipeline/observation_execution.h
+++ include/citlali/core/pipeline/observation_execution.h
@@ -10,6 +10,7 @@
 #include <citlali/core/pipeline/observation_preflight.h>
 #include <citlali/core/pipeline/observation_pipeline.h>
 #include <citlali/core/pipeline/reduction_iteration_outputs.h>
+#include <citlali/core/pipeline/reduction_observation.h>
 #include <citlali/core/pipeline/reduction_observation_inputs.h>
 #include <citlali/core/pipeline/reduction_observation_pipeline.h>
 #include <citlali/core/pipeline/output_layout.h>
@@ -20,27 +21,6 @@
 #include <utility>
 
 namespace citlali::pipeline {
-
-template <bool IsBeammap, auto RawObsMap, auto FilteredObsMap, bool FitMaps,
-          class TodProc, class KidsProc, class RawObs, class RawObsKidsMeta,
-          class MapExtents, class MapCoords, class DateObs, class Logger>
-bool run_reduction_observation(
-    TodProc &todproc, KidsProc &kidsproc, const RawObs &rawobs,
-    const RawObsKidsMeta &rawobs_kids_meta, bool has_multiple_inputs,
-    MapExtents &map_extents, MapCoords &map_coords,
-    std::size_t observation_index, DateObs &&date_obs, const Logger &logger) {
-    if (!prepare_reduction_observation_inputs<IsBeammap>(
-            todproc, rawobs, rawobs_kids_meta, has_multiple_inputs,
-            map_extents, map_coords, observation_index,
-            std::forward<DateObs>(date_obs), logger)) {
-        return false;
-    }
-
-    run_reduction_observation_pipeline<IsBeammap, RawObsMap, FilteredObsMap,
-                                       FitMaps>(
-        todproc, kidsproc, rawobs, logger);
-    return true;
-}
 
 template <bool IsBeammap, auto RawObsMap, auto FilteredObsMap, bool FitMaps,
           class KidsDataProc, class TodProc, class IOCoordinator,
