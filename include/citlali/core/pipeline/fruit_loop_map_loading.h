4f20a0710e3c49145113b077c56b0c0db6e5978a
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 15:40:13 -0400

    extract observation fruit loop map loading helper

--- include/citlali/core/pipeline/fruit_loop_map_loading.h
+++ include/citlali/core/pipeline/fruit_loop_map_loading.h
@@ -1,17 +1,5 @@
 #pragma once
 
 #include <citlali/core/pipeline/initial_fruit_loop_map_loading.h>
+#include <citlali/core/pipeline/observation_fruit_loop_map_loading.h>
 #include <citlali/core/pipeline/previous_fruit_loop_map_loading.h>
-
-namespace citlali::pipeline {
-
-template <bool IsBeammap, class Engine, class Logger>
-void load_observation_fruit_loop_maps_if_needed(Engine &engine,
-                                                const Logger &logger) {
-    if constexpr (!IsBeammap) {
-        load_initial_fruit_loop_maps_if_requested(engine);
-        load_previous_fruit_loop_maps_if_needed(engine, logger);
-    }
-}
-
-}  // namespace citlali::pipeline
