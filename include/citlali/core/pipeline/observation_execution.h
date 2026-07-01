95688993387a85c80e989a067b80ae31ff5cc305
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 09:51:29 -0400

    move raw observation output helpers

--- include/citlali/core/pipeline/observation_execution.h
+++ include/citlali/core/pipeline/observation_execution.h
@@ -2,6 +2,7 @@
 
 #include <citlali/core/pipeline/fruit_loop_map_loading.h>
 #include <citlali/core/pipeline/iteration_lifecycle.h>
+#include <citlali/core/pipeline/observation_outputs.h>
 #include <citlali/core/pipeline/observation_preflight.h>
 #include <citlali/core/pipeline/output_layout.h>
 #include <citlali/core/pipeline/output_policy.h>
@@ -252,38 +253,6 @@ void coadd_observation(TodProc &todproc, const Logger &logger) {
     if (!engine.rtcproc.run_polarization) {
         todproc.coadd();
     }
-}
-
-template <class Engine, class Logger>
-void calculate_raw_observation_noise_products_if_needed(
-    Engine &engine, const Logger &logger) {
-    if (engine.run_mapmaking &&
-        engine.run_noise_products &&
-        engine.run_noise) {
-        logger->info("calculating raw obs empirical noise products");
-        engine.omb.calc_noise_products(engine.apply_empirical_noise_weights);
-    }
-}
-
-template <auto RawObsMap, class Engine, class Logger>
-void output_raw_observation_maps_if_needed(Engine &engine,
-                                           const Logger &logger) {
-    if (engine.run_mapmaking) {
-        engine.create_obs_map_files();
-        logger->info("outputting raw obs files");
-        engine.template output<RawObsMap>();
-    }
-    else {
-        logger->info("mapmaking disabled; skipping raw obs map output");
-    }
-}
-
-template <auto RawObsMap, class TodProc, class Logger>
-void write_raw_observation_outputs(TodProc &todproc, const Logger &logger) {
-    auto &engine = todproc.engine();
-
-    calculate_raw_observation_noise_products_if_needed(engine, logger);
-    output_raw_observation_maps_if_needed<RawObsMap>(engine, logger);
 }
 
 template <auto FilteredObsMap, class Engine, class Logger>
