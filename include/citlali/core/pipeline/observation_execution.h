44725f603b7aa78f85b5abc1ec4ad5d51e77b9b7
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 09:56:14 -0400

    move raw coadd output helpers

--- include/citlali/core/pipeline/observation_execution.h
+++ include/citlali/core/pipeline/observation_execution.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <citlali/core/pipeline/coadd_outputs.h>
 #include <citlali/core/pipeline/fruit_loop_map_loading.h>
 #include <citlali/core/pipeline/iteration_lifecycle.h>
 #include <citlali/core/pipeline/observation_outputs.h>
@@ -270,59 +271,6 @@ void write_observation_outputs_and_accumulate(TodProc &todproc,
         write_filtered_observation_outputs_if_needed<FilteredObsMap, FitMaps>(
             todproc, logger);
     }
-}
-
-template <class TodProc, class Logger>
-void prepare_raw_coadd_maps(TodProc &todproc, const Logger &logger) {
-    auto &engine = todproc.engine();
-
-    logger->debug("creating cmb filenames");
-    todproc.create_coadded_map_files();
-
-    logger->info("normalizing coadded maps");
-    if (engine.rtcproc.run_polarization) {
-        engine.cmb.normalize_polarized_maps();
-    }
-    else {
-        engine.cmb.normalize_maps();
-    }
-}
-
-template <class Engine, class Logger>
-void calculate_raw_coadd_noise_products_if_needed(
-    Engine &engine, const Logger &logger) {
-    if (engine.run_noise_products && engine.run_noise) {
-        logger->info("calculating raw coadd empirical noise products");
-        engine.cmb.calc_noise_products(engine.apply_empirical_noise_weights);
-    }
-}
-
-template <class Engine, class Logger>
-void calculate_raw_coadd_map_diagnostics(Engine &engine,
-                                         const Logger &logger) {
-    logger->info("calculating coadded map psd");
-    engine.cmb.calc_map_psd();
-    logger->info("calculating coadded map histogram");
-    engine.cmb.calc_map_hist();
-
-    engine.cmb.calc_median_err();
-    engine.cmb.calc_median_rms();
-}
-
-template <auto RawCoaddMap, class Engine, class Logger>
-void output_raw_coadd_maps(Engine &engine, const Logger &logger) {
-    logger->info("outputting raw coadded files");
-    engine.template output<RawCoaddMap>();
-}
-
-template <auto RawCoaddMap, class TodProc, class Logger>
-void write_raw_coadd_outputs(TodProc &todproc, const Logger &logger) {
-    auto &engine = todproc.engine();
-
-    prepare_raw_coadd_maps(todproc, logger);
-    calculate_raw_coadd_noise_products_if_needed(engine, logger);
-    calculate_raw_coadd_map_diagnostics(engine, logger);
-    output_raw_coadd_maps<RawCoaddMap>(engine, logger);
 }
 
 template <auto FilteredCoaddMap, class Engine, class Logger>
