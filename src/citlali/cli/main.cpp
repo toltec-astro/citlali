b78318bf80a207f63533ac587f4c68dd87c9536e
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 16:18:59 -0400

    remove obsolete cli parser includes

--- src/citlali/cli/main.cpp
+++ src/citlali/cli/main.cpp
@@ -5,19 +5,10 @@
 #include <kids/sweep/fitter.h>
 #include <kids/timestream/solver.h>
 #include <kids/toltec/toltec.h>
-#include <kidscpp_config/gitversion.h>
-#include <tula_config/gitversion.h>
-#include <tula/cli.h>
 #include <tula/config/core.h>
-#include <tula/config/flatconfig.h>
 #include <tula/config/yamlconfig.h>
-#include <tula/enum.h>
-#include <tula/filesystem.h>
 #include <tula/formatter/container.h>
-#include <tula/formatter/enum.h>
-#include <tula/grppi.h>
 #include <tula/logging.h>
-#include <tula/switch_invoke.h>
 
 #include <cstdlib>
 #include <cmath>
