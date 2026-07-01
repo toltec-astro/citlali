57f3c114df3cefdb0f38d8472be1b78f64ff76d5
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 17:03:47 -0400

    add standard reduction select and run helper

--- include/citlali/core/cli/standard_reduction_execution.h
+++ include/citlali/core/cli/standard_reduction_execution.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <citlali/core/cli/reduction_execution.h>
+#include <citlali/core/cli/standard_reduction_selection.h>
 #include <citlali/core/cli/standard_reduction_types.h>
 
+#include <cstdlib>
 #include <ostream>
 
 namespace citlali::cli {
@@ -16,6 +18,22 @@ int run_standard_citlali_reduction_variant(
     return run_standard_cli_reduction_variant<
         StandardBeammapTodProcessor, StandardPointingTodProcessor,
         KidsDataProc>(todproc, co, config, config_filepaths, logger, os);
+}
+
+template <class KidsDataProc, class IOCoordinator, class Config,
+          class ConfigFilepaths, class Logger>
+int select_and_run_standard_citlali_reduction(
+    const IOCoordinator &co, Config &config,
+    const ConfigFilepaths &config_filepaths, const Logger &logger,
+    std::ostream &os) {
+    StandardTodProcessorVariant todproc;
+    if (!select_standard_citlali_tod_processor_or_report_failure(
+            todproc, config, logger, os)) {
+        return EXIT_FAILURE;
+    }
+
+    return run_standard_citlali_reduction_variant<KidsDataProc>(
+        todproc, co, config, config_filepaths, logger, os);
 }
 
 }  // namespace citlali::cli
