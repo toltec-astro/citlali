af6a4b6e50e15be5cf259969ba5b6fec7d8abdb4
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 13:43:59 -0400

    extract observation exposure helper

---
+++ include/citlali/core/pipeline/observation_exposure_time.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace citlali::pipeline {
+
+template <class Engine>
+void update_observation_exposure_time(Engine &engine) {
+    auto t0 = engine.telescope.tel_data["TelTime"](0);
+    auto tn = engine.telescope.tel_data["TelTime"](
+        engine.telescope.tel_data["TelTime"].size() - 1);
+
+    engine.omb.exposure_time = tn - t0;
+    if (engine.run_coadd) {
+        engine.cmb.exposure_time =
+            engine.cmb.exposure_time + engine.omb.exposure_time;
+    }
+}
+
+}  // namespace citlali::pipeline
--- include/citlali/core/pipeline/observation_timing.h
+++ include/citlali/core/pipeline/observation_timing.h
@@ -1,21 +1,10 @@
 #pragma once
 
+#include <citlali/core/pipeline/observation_exposure_time.h>
+
 #include <utility>
 
 namespace citlali::pipeline {
-
-template <class Engine>
-void update_observation_exposure_time(Engine &engine) {
-    auto t0 = engine.telescope.tel_data["TelTime"](0);
-    auto tn = engine.telescope.tel_data["TelTime"](
-        engine.telescope.tel_data["TelTime"].size() - 1);
-
-    engine.omb.exposure_time = tn - t0;
-    if (engine.run_coadd) {
-        engine.cmb.exposure_time =
-            engine.cmb.exposure_time + engine.omb.exposure_time;
-    }
-}
 
 template <class Engine, class DateObs>
 void append_observation_date(Engine &engine, DateObs &&date_obs) {
