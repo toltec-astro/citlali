a1102bb0280375522715b6dad5b8e6c0d0c6699a
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 11:42:42 -0400

    make observation execution a facade header

--- include/citlali/core/pipeline/observation_execution.h
+++ include/citlali/core/pipeline/observation_execution.h
@@ -1,29 +1,3 @@
 #pragma once
 
-#include <citlali/core/pipeline/coadd_outputs.h>
-#include <citlali/core/pipeline/fruit_loop_map_loading.h>
-#include <citlali/core/pipeline/initial_reduction_geometry.h>
-#include <citlali/core/pipeline/iteration_buffers.h>
-#include <citlali/core/pipeline/iteration_lifecycle.h>
-#include <citlali/core/pipeline/observation_buffers.h>
-#include <citlali/core/pipeline/observation_outputs.h>
-#include <citlali/core/pipeline/observation_preflight.h>
-#include <citlali/core/pipeline/observation_pipeline.h>
-#include <citlali/core/pipeline/reduction_iteration.h>
-#include <citlali/core/pipeline/reduction_iteration_loop.h>
-#include <citlali/core/pipeline/reduction_iteration_outputs.h>
-#include <citlali/core/pipeline/reduction_observation.h>
-#include <citlali/core/pipeline/reduction_observation_inputs.h>
-#include <citlali/core/pipeline/reduction_observation_loop.h>
-#include <citlali/core/pipeline/reduction_observation_pipeline.h>
 #include <citlali/core/pipeline/reduction_pipeline.h>
-#include <citlali/core/pipeline/output_layout.h>
-#include <citlali/core/pipeline/output_policy.h>
-#include <citlali/core/pipeline/reduction_iteration_setup.h>
-
-#include <cstddef>
-#include <utility>
-
-namespace citlali::pipeline {
-
-}  // namespace citlali::pipeline
