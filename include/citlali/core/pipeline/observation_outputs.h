fd1bc1a7bdf78d8a934a6fb3baa27ccefb2eaa08
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 15:25:16 -0400

    make observation outputs a facade

--- include/citlali/core/pipeline/observation_outputs.h
+++ include/citlali/core/pipeline/observation_outputs.h
@@ -1,7 +1,3 @@
 #pragma once
 
-#include <citlali/core/pipeline/filtered_observation_outputs.h>
-#include <citlali/core/pipeline/observation_coadd_accumulation.h>
 #include <citlali/core/pipeline/observation_output_execution.h>
-#include <citlali/core/pipeline/output_policy.h>
-#include <citlali/core/pipeline/raw_observation_outputs.h>
