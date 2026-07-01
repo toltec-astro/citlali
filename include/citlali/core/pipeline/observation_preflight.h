854b7363cee1303d131f163ffbeab85c6ecb33d6
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-01 13:03:55 -0400

    use direct preflight calibration includes

--- include/citlali/core/pipeline/observation_preflight.h
+++ include/citlali/core/pipeline/observation_preflight.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <citlali/core/pipeline/flux_calibration.h>
 #include <citlali/core/pipeline/fruit_loop_iteration_policy.h>
+#include <citlali/core/pipeline/hwpr_loading.h>
 #include <citlali/core/pipeline/kids_metadata.h>
-#include <citlali/core/pipeline/observation_calibration.h>
+#include <citlali/core/pipeline/observation_calibration_config.h>
 #include <citlali/core/pipeline/observation_detector_diagnostics.h>
 #include <citlali/core/pipeline/observation_input_checks.h>
 #include <citlali/core/pipeline/observation_sample_rate.h>
 #include <citlali/core/pipeline/observation_telescope.h>
 #include <citlali/core/pipeline/observation_timing.h>
 #include <citlali/core/pipeline/rawobs_data_items.h>
+#include <citlali/core/pipeline/reduction_observation_calibration.h>
 #include <citlali/core/pipeline/reduction_config.h>
