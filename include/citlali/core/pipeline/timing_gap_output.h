19f941f12f7d2d24a7d949c9673d9e976ba766d4
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 10:50:56 -0400

    use timing gap entry log helper

--- include/citlali/core/pipeline/timing_gap_output.h
+++ include/citlali/core/pipeline/timing_gap_output.h
@@ -45,7 +45,7 @@ void record_timing_gaps_if_needed(const Engine &engine, const Logger &logger) {
             f.open(gaps_log_filepath(engine.obsnum_dir_name));
             write_timing_gaps_log_header(f);
             for (auto const &[key, val] : engine.gaps) {
-                logger->debug("{} gaps: {}", key, val);
+                log_timing_gap_entry(key, val, logger);
                 f << "-" + key + " gaps: " << val << "\n";
             }
             f.close();
