9ce0f76570d165fc8128a1a5ac0a3fb9ad705d93
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 09:45:23 -0400

    add observation coadd execution gate

--- include/citlali/core/pipeline/observation_coadd_accumulation.h
+++ include/citlali/core/pipeline/observation_coadd_accumulation.h
@@ -2,6 +2,11 @@
 
 namespace citlali::pipeline {
 
+template <class Engine>
+bool should_run_observation_coadd(const Engine &engine) {
+    return !engine.rtcproc.run_polarization;
+}
+
 template <class TodProc, class Logger>
 void coadd_observation(TodProc &todproc, const Logger &logger) {
     auto &engine = todproc.engine();
