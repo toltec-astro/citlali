1f09ace82823f7147d8b47b9f86302f0c139ce0c
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 13:06:05 -0400

    add shared map noise product helper

---
+++ include/citlali/core/pipeline/map_noise_products.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace citlali::pipeline {
+
+template <class MapBuffer, class Logger>
+void calculate_map_noise_products_with_log(
+    MapBuffer &map_buffer, bool apply_empirical_noise_weights,
+    const Logger &logger, const char *log_message) {
+    logger->info("{}", log_message);
+    map_buffer.calc_noise_products(apply_empirical_noise_weights);
+}
+
+}  // namespace citlali::pipeline
