c86ed447ffc4e4e31b56709da81a100f376fd72a
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 10:49:16 -0400

    use timing gap warning helper

--- include/citlali/core/pipeline/timing_gap_output.h
+++ include/citlali/core/pipeline/timing_gap_output.h
@@ -28,8 +28,7 @@ void warn_timing_gaps_found(const Engine &engine, const Logger &logger) {
 template <class Engine, class Logger>
 void record_timing_gaps_if_needed(const Engine &engine, const Logger &logger) {
     if (has_timing_gaps(engine)) {
-        logger->warn("gaps found in obnsum {} data file timing!",
-                     engine.obsnum);
+        warn_timing_gaps_found(engine, logger);
         if (should_write_timing_gaps_log(engine)) {
             logger->debug("writing gaps.log file");
             std::ofstream f;
