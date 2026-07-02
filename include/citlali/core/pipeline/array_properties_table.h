9d95163bb4f88aa38904383cbf7edb94b252ee62
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 11:37:41 -0400

    add array properties table log helper

--- include/citlali/core/pipeline/array_properties_table.h
+++ include/citlali/core/pipeline/array_properties_table.h
@@ -32,6 +32,12 @@ std::vector<std::string> raw_kids_interfaces(const RawObs &rawobs) {
     return result;
 }
 
+template <class Logger>
+void log_array_properties_table_filepath(const std::string &filepath,
+                                         const Logger &logger) {
+    logger->info("getting array properties table {}", filepath);
+}
+
 template <class Engine, class RawObs, class Logger>
 void load_array_properties_table(Engine &engine, const RawObs &rawobs,
                                  const Logger &logger) {
