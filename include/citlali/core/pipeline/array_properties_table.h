7c4faaf903801973e1165cb3e9ee9cdb0c91cf9d
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 11:36:28 -0400

    use array properties table filepath helper

--- include/citlali/core/pipeline/array_properties_table.h
+++ include/citlali/core/pipeline/array_properties_table.h
@@ -15,7 +15,7 @@ std::string array_properties_table_filepath(const RawObs &rawobs) {
 template <class Engine, class RawObs, class Logger>
 void load_array_properties_table(Engine &engine, const RawObs &rawobs,
                                  const Logger &logger) {
-    auto apt_path = rawobs.array_prop_table().filepath();
+    auto apt_path = array_properties_table_filepath(rawobs);
     logger->info("getting array properties table {}", apt_path);
 
     std::vector<std::string> raw_filenames, interfaces;
