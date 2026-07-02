967f2bb2808e79107de0dc61d5f904225ad851da
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 15:09:00 -0400

    reuse RTC diag stream network int helper

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -6279,12 +6279,9 @@ void Engine::create_tod_files() {
                 n_rtc_stream_nw_values, fill_double);
         };
         auto add_rtc_nw_int = [&](const std::string &name, const std::string &comment) {
-            netCDF::NcVar v = fo.addVar(name, netCDF::ncInt, rtc_nw_dims);
-            v.putAtt("units", "N/A");
-            v.putAtt("comment", comment);
-            std::vector<int> init(static_cast<std::size_t>(n_tod_output_scans_for_stream) *
-                                  static_cast<std::size_t>(calib.n_nws), fill_int);
-            v.putVar(init.data());
+            citlali::pipeline::add_rtcdiag_network_int(
+                fo, name, comment, rtc_nw_dims, rtc_stream_no_chunks,
+                n_rtc_stream_nw_values, fill_int);
         };
 
         add_rtc_nw_int("rtc_network_n_det_input",
