81d84495ff9531a93af0d9477a88da90315e616d
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 11:39:47 -0400

    use raw apt creation helper

--- include/citlali/core/pipeline/observation_calibration_config.h
+++ include/citlali/core/pipeline/observation_calibration_config.h
@@ -39,8 +39,7 @@ void configure_observation_calibration(TodProc &todproc, const RawObs &rawobs,
     if constexpr (IsBeammap) {
         load_photometry_config(engine, rawobs);
         if (should_make_apt_from_raw_files(engine)) {
-            logger->info("making apt file from raw nc files");
-            todproc.get_apt_from_files(rawobs);
+            make_apt_from_raw_files(todproc, rawobs, logger);
             return;
         }
     }
