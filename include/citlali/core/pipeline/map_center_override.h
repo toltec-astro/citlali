2d8b3a4cf55bb87fc31af6adcdd8e219b0baf067
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 12:10:13 -0400

    add map center header setter

--- include/citlali/core/pipeline/map_center_override.h
+++ include/citlali/core/pipeline/map_center_override.h
@@ -30,6 +30,13 @@ void log_map_center_override(double ra_degrees, double dec_degrees,
                  ra_degrees, dec_degrees);
 }
 
+template <class Engine>
+void set_map_center_header(Engine &engine, double ra_radians,
+                           double dec_radians) {
+    engine.telescope.tel_header["Header.Source.Ra"].setConstant(ra_radians);
+    engine.telescope.tel_header["Header.Source.Dec"].setConstant(dec_radians);
+}
+
 template <class Engine, class Logger>
 void overwrite_map_center_if_configured(Engine &engine, const Logger &logger) {
     if (has_map_center_override(engine)) {
