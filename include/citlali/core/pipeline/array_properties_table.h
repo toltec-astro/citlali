75596b602767d90fef617eec182944edba0a5a9c
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 13:48:31 -0400

    split array properties table source helpers

--- include/citlali/core/pipeline/array_properties_table.h
+++ include/citlali/core/pipeline/array_properties_table.h
@@ -1,30 +1,11 @@
 #pragma once
 
+#include <citlali/core/pipeline/array_properties_table_source.h>
 #include <citlali/core/pipeline/raw_kids_data_access.h>
 
-#include <string>
 #include <vector>
 
 namespace citlali::pipeline {
-
-template <class RawObs>
-std::string array_properties_table_filepath(const RawObs &rawobs) {
-    return rawobs.array_prop_table().filepath();
-}
-
-template <class Logger>
-void log_array_properties_table_filepath(const std::string &filepath,
-                                         const Logger &logger) {
-    logger->info("getting array properties table {}", filepath);
-}
-
-template <class Engine>
-void load_array_properties_table_file(
-    Engine &engine, const std::string &filepath,
-    std::vector<std::string> &raw_filenames,
-    std::vector<std::string> &interfaces) {
-    engine.calib.get_apt(filepath, raw_filenames, interfaces);
-}
 
 template <class Engine, class RawObs, class Logger>
 void load_array_properties_table(Engine &engine, const RawObs &rawobs,
---
+++ include/citlali/core/pipeline/array_properties_table_source.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace citlali::pipeline {
+
+template <class RawObs>
+std::string array_properties_table_filepath(const RawObs &rawobs) {
+    return rawobs.array_prop_table().filepath();
+}
+
+template <class Logger>
+void log_array_properties_table_filepath(const std::string &filepath,
+                                         const Logger &logger) {
+    logger->info("getting array properties table {}", filepath);
+}
+
+template <class Engine>
+void load_array_properties_table_file(
+    Engine &engine, const std::string &filepath,
+    std::vector<std::string> &raw_filenames,
+    std::vector<std::string> &interfaces) {
+    engine.calib.get_apt(filepath, raw_filenames, interfaces);
+}
+
+}  // namespace citlali::pipeline
