4f503c6e31c7f409fb7a9ab4c067534027c261e7
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 10:29:44 -0400

    use filtered observation output dir gate

--- include/citlali/core/pipeline/observation_output_dirs.h
+++ include/citlali/core/pipeline/observation_output_dirs.h
@@ -20,11 +20,9 @@ void create_observation_output_dirs(const Engine &engine,
     logger->debug("creating obsnum raw directory");
     fs::create_directories(engine.obsnum_dir_name + "raw/");
 
-    if (!engine.run_coadd) {
-        if (engine.run_map_filter) {
-            logger->debug("creating obsnum filtered directory");
-            fs::create_directories(engine.obsnum_dir_name + "filtered/");
-        }
+    if (should_create_filtered_observation_output_dir(engine)) {
+        logger->debug("creating obsnum filtered directory");
+        fs::create_directories(engine.obsnum_dir_name + "filtered/");
     }
     if (engine.verbose_mode) {
         logger->debug("creating obsnum logs directory");
