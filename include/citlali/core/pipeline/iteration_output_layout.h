562bc1f8c0c02144be8a5dde567479a94ad8cb5d
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 10:15:04 -0400

    add iteration config copy helper

--- include/citlali/core/pipeline/iteration_output_layout.h
+++ include/citlali/core/pipeline/iteration_output_layout.h
@@ -2,6 +2,8 @@
 
 #include <citlali/core/pipeline/output_config_copy.h>
 
+#include <string>
+
 namespace citlali::pipeline {
 
 template <class Engine>
@@ -12,6 +14,14 @@ bool should_prepare_iteration_output_layout(const Engine &engine) {
 template <class TodProc>
 void create_iteration_output_directory(TodProc &todproc) {
     todproc.create_output_dir();
+}
+
+template <class ConfigFilepaths, class Logger>
+void copy_iteration_config_files(const ConfigFilepaths &config_filepaths,
+                                 const std::string &reduction_dir,
+                                 const Logger &logger) {
+    copy_config_files_to_reduction_dir(config_filepaths, reduction_dir,
+                                       logger);
 }
 
 template <class TodProc, class ConfigFilepaths, class Logger>
