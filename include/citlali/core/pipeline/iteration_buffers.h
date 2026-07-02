e90895c225313f2149351bfecdebabe45cf67ee5
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 10:18:00 -0400

    add coadd map buffer helper

--- include/citlali/core/pipeline/iteration_buffers.h
+++ include/citlali/core/pipeline/iteration_buffers.h
@@ -8,6 +8,12 @@ bool should_allocate_coadd_noise_buffer(const Engine &engine) {
 }
 
 template <class TodProc, class Logger>
+void allocate_coadd_map_buffer(TodProc &todproc, const Logger &logger) {
+    logger->info("allocating cmb");
+    todproc.allocate_cmb();
+}
+
+template <class TodProc, class Logger>
 void prepare_coadd_iteration_buffers(TodProc &todproc, const Logger &logger) {
     auto &engine = todproc.engine();
 
