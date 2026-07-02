1749a06baccc691c1417b579a11d4dec1af7ef0d
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 10:16:25 -0400

    use single config copy helper

--- include/citlali/core/pipeline/output_config_copy.h
+++ include/citlali/core/pipeline/output_config_copy.h
@@ -35,12 +35,9 @@ template <class Logger>
 void copy_config_files_to_reduction_dir(
     const std::vector<std::string> &config_filepaths,
     const std::string &reduction_dir, const Logger &logger) {
-    namespace fs = std::filesystem;
     for (const auto &config_filepath : config_filepaths) {
-        logger->debug("copying config files into redu directory");
-        fs::copy(config_filepath,
-                 config_copy_destination(reduction_dir, config_filepath),
-                 fs::copy_options::overwrite_existing);
+        copy_config_file_to_reduction_dir(config_filepath, reduction_dir,
+                                          logger);
     }
 }
 
