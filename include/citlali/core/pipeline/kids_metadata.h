ef38922f5d93fecb331c7a7941a2c7ebcbdbac3e
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 08:43:52 -0400

    add observation kids processor factory

--- include/citlali/core/pipeline/kids_metadata.h
+++ include/citlali/core/pipeline/kids_metadata.h
@@ -14,6 +14,11 @@ auto make_kids_data_proc(Config &config) {
     return KidsDataProc::from_config(config.get_config("kids"));
 }
 
+template <class KidsDataProc, class Config>
+auto make_reduction_observation_kids_proc(Config &config) {
+    return make_kids_data_proc<KidsDataProc>(config);
+}
+
 template <class Engine, class RawObsKidsMeta, class Logger>
 void update_sample_rate_from_rawobs_meta(Engine &engine,
                                          const RawObsKidsMeta &rawobs_kids_meta,
