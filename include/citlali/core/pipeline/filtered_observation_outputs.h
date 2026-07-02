89cd59c38cdb3f8ecc144a19e8728d2713354258
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 09:50:22 -0400

    use filtered observation source finder gate

--- include/citlali/core/pipeline/filtered_observation_outputs.h
+++ include/citlali/core/pipeline/filtered_observation_outputs.h
@@ -49,7 +49,7 @@ void calculate_filtered_observation_map_diagnostics(Engine &engine,
 template <auto FilteredObsMap, bool FitMaps, class Engine, class Logger>
 void find_and_fit_filtered_observation_maps_if_needed(
     Engine &engine, const Logger &logger) {
-    if (engine.run_source_finder) {
+    if (should_find_filtered_observation_sources(engine)) {
         logger->info("finding filtered obs map sources");
         engine.template find_sources<FilteredObsMap>(engine.omb);
     }
