37cee4a7ed2fdb790f3c7497ac5ef146f819d005
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 15:19:48 -0400

    reuse RTC diag stream impulsive snippet double helper

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -6345,12 +6345,10 @@ void Engine::create_tod_files() {
                     fill_int);
             };
             auto add_rtc_imp_snip_double = [&](const std::string &name, const std::string &comment) {
-                netCDF::NcVar v = fo.addVar(name, netCDF::ncDouble, rtc_impulsive_snippet_dims);
-                v.putAtt("units", "N/A");
-                v.putAtt("comment", comment);
-                std::vector<double> init(static_cast<std::size_t>(n_tod_output_scans_for_stream) *
-                                         static_cast<std::size_t>(calib.n_nws) * n_slots * n_snippet, fill_double);
-                v.putVar(init.data());
+                citlali::pipeline::add_rtcdiag_impulsive_snippet_double(
+                    fo, name, comment, rtc_impulsive_snippet_dims,
+                    rtc_stream_no_chunks, n_rtc_stream_impulsive_snippet_values,
+                    fill_double);
             };
             auto add_rtc_imp_snip_int = [&](const std::string &name, const std::string &comment) {
                 netCDF::NcVar v = fo.addVar(name, netCDF::ncInt, rtc_impulsive_snippet_dims);
