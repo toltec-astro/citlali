1f704f84be8a52019401ae68ed92427f0c4d8586
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 12:25:59 -0400

    add calibration flux scale helper

--- include/citlali/core/pipeline/flux_calibration.h
+++ include/citlali/core/pipeline/flux_calibration.h
@@ -17,6 +17,12 @@ void log_flux_calibration(const Logger &logger) {
     logger->info("calculating flux calibration");
 }
 
+template <class Engine>
+void calculate_calibration_flux_scale(Engine &engine) {
+    engine.calib.calc_flux_calibration(flux_calibration_signal_unit(engine),
+                                       flux_calibration_pixel_size_rad(engine));
+}
+
 template <class Engine, class Logger>
 void calculate_flux_calibration(Engine &engine, const Logger &logger) {
     log_flux_calibration(logger);
