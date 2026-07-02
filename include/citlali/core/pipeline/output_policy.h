bf88141e0869be67d0b8a64d797f2754a801319d
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 12:57:59 -0400

    add shared filtered output policy helpers

--- include/citlali/core/pipeline/output_policy.h
+++ include/citlali/core/pipeline/output_policy.h
@@ -8,6 +8,23 @@ bool should_write_filtered_outputs(const Engine &engine) {
 }
 
 template <class Engine>
+bool filtered_maps_written_during_filtering(const Engine &engine) {
+    return engine.write_filtered_maps_partial;
+}
+
+template <class Engine>
+bool should_calculate_filtered_noise_products(const Engine &engine) {
+    return engine.run_noise_products &&
+           engine.run_noise &&
+           !filtered_maps_written_during_filtering(engine);
+}
+
+template <class Engine>
+bool should_find_filtered_sources(const Engine &engine) {
+    return engine.run_source_finder;
+}
+
+template <class Engine>
 bool should_write_iteration_coadd_outputs(const Engine &engine) {
     return engine.run_coadd;
 }
