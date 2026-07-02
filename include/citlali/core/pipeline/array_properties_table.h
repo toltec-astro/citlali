5e805e6fece595fa8f715a9f20ea03cdd59e3bd9
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 11:36:44 -0400

    add raw kids filepath collector

--- include/citlali/core/pipeline/array_properties_table.h
+++ include/citlali/core/pipeline/array_properties_table.h
@@ -12,6 +12,16 @@ std::string array_properties_table_filepath(const RawObs &rawobs) {
     return rawobs.array_prop_table().filepath();
 }
 
+template <class RawObs>
+std::vector<std::string> raw_kids_filepaths(const RawObs &rawobs) {
+    std::vector<std::string> result;
+    for (const auto &data_item : rawobs.kidsdata()) {
+        const auto &item = detail::unwrap_reference_wrapper(data_item);
+        result.push_back(item.filepath());
+    }
+    return result;
+}
+
 template <class Engine, class RawObs, class Logger>
 void load_array_properties_table(Engine &engine, const RawObs &rawobs,
                                  const Logger &logger) {
