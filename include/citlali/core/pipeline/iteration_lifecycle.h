439fa366eb33d59f1efbf8cdb62cebd5aa3101f5
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 13:28:00 -0400

    split reduction learning lifecycle helpers

--- include/citlali/core/pipeline/iteration_lifecycle.h
+++ include/citlali/core/pipeline/iteration_lifecycle.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <citlali/core/pipeline/reduction_learning_lifecycle.h>
+
 namespace citlali::pipeline {
 
 struct ReductionIterationState {
@@ -30,29 +32,6 @@ bool should_log_fruit_loop_iteration_start(const Engine &engine) {
 }
 
 template <class Engine>
-bool fruit_loop_learning_source_model_available(const Engine &engine) {
-    return engine.ptcproc.run_fruit_loops &&
-           (engine.fruit_iter > 0 ||
-            engine.ptcproc.fruit_loops_path != "null");
-}
-
-template <class Engine>
-bool should_log_reduction_learning_diagnostics(const Engine &engine) {
-    return engine.reduction_learning.is_enabled() &&
-           engine.reduction_learning.diagnostics_enabled();
-}
-
-template <class Engine, class Logger>
-void log_reduction_learning_iteration_if_needed(Engine &engine,
-                                                const Logger &logger,
-                                                const char *phase) {
-    if (should_log_reduction_learning_diagnostics(engine)) {
-        logger->info("reduction learning {}: {}",
-                     phase, engine.reduction_learning.summary_string());
-    }
-}
-
-template <class Engine>
 void begin_iteration_weight_validation(Engine &engine) {
     engine.ptcproc.begin_weight_validation_iteration(engine.fruit_iter);
 }
@@ -60,19 +39,6 @@ void begin_iteration_weight_validation(Engine &engine) {
 template <class Engine>
 void finalize_iteration_weight_validation(Engine &engine) {
     engine.ptcproc.finalize_weight_validation_iteration(engine.fruit_iter);
-}
-
-template <class Engine>
-void begin_reduction_learning_iteration(Engine &engine) {
-    const bool learning_source_model_available =
-        fruit_loop_learning_source_model_available(engine);
-    engine.reduction_learning.begin_iteration(
-        engine.fruit_iter, learning_source_model_available, engine.redu_type);
-}
-
-template <class Engine>
-void finalize_reduction_learning_iteration(Engine &engine) {
-    engine.reduction_learning.finalize_iteration(engine.fruit_iter);
 }
 
 template <class Engine, class Logger>
