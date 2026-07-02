281870fae7509b60af5c839fe796af5ab429909d
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 11:02:49 -0400

    use hwpr data presence helper

--- include/citlali/core/pipeline/hwpr_loading.h
+++ include/citlali/core/pipeline/hwpr_loading.h
@@ -24,7 +24,7 @@ void load_hwpr_data_if_requested(Engine &engine, const RawObs &rawobs,
                                  const Logger &logger) {
     if (should_load_hwpr_for_polarization(engine)) {
         std::string hwpr_filepath;
-        if (rawobs.hwpdata().has_value() && !is_hwpr_ignored_by_config(engine)) {
+        if (has_hwpr_data(rawobs) && !is_hwpr_ignored_by_config(engine)) {
             hwpr_filepath = rawobs.hwpdata()->filepath();
             if (hwpr_filepath != "null") {
                 logger->info("getting hwpr file {}", hwpr_filepath);
