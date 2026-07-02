5f48f945e4af6554d1223c4deaf946754a906bb4
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 12:52:15 -0400

    use shared noise policy for filtered coadds

--- include/citlali/core/pipeline/filtered_coadd_outputs.h
+++ include/citlali/core/pipeline/filtered_coadd_outputs.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <citlali/core/pipeline/noise_weight_policy.h>
 #include <citlali/core/pipeline/output_policy.h>
 
 namespace citlali::pipeline {
@@ -25,8 +26,7 @@ bool filtered_coadd_maps_written_during_filtering(const Engine &engine) {
 template <class Engine>
 bool filtered_coadd_noise_products_apply_empirical_weights(
     const Engine &engine) {
-    return engine.apply_empirical_noise_weights ||
-           engine.wiener_filter.normalize_error;
+    return filtered_noise_products_apply_empirical_weights(engine);
 }
 
 template <auto FilteredCoaddMap, class Engine, class Logger>
