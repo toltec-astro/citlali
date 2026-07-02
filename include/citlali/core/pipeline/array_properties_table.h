c619c29a2d80d0e7ab8ffbebbd6790bb1e8eb034
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 11:38:09 -0400

    add array properties table file load helper

--- include/citlali/core/pipeline/array_properties_table.h
+++ include/citlali/core/pipeline/array_properties_table.h
@@ -38,6 +38,14 @@ void log_array_properties_table_filepath(const std::string &filepath,
     logger->info("getting array properties table {}", filepath);
 }
 
+template <class Engine>
+void load_array_properties_table_file(
+    Engine &engine, const std::string &filepath,
+    const std::vector<std::string> &raw_filenames,
+    const std::vector<std::string> &interfaces) {
+    engine.calib.get_apt(filepath, raw_filenames, interfaces);
+}
+
 template <class Engine, class RawObs, class Logger>
 void load_array_properties_table(Engine &engine, const RawObs &rawobs,
                                  const Logger &logger) {
