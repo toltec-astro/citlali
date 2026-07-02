8ad03e7e95d9bc52c7f59f6cd66939c6a52f8426
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 13:09:27 -0400

    use shared source helper for observations

--- include/citlali/core/pipeline/filtered_observation_outputs.h
+++ include/citlali/core/pipeline/filtered_observation_outputs.h
@@ -3,6 +3,7 @@
 #include <citlali/core/pipeline/map_diagnostics.h>
 #include <citlali/core/pipeline/map_filtering.h>
 #include <citlali/core/pipeline/map_noise_products.h>
+#include <citlali/core/pipeline/map_source_finding.h>
 #include <citlali/core/pipeline/noise_weight_policy.h>
 #include <citlali/core/pipeline/output_policy.h>
 
@@ -68,8 +69,8 @@ template <auto FilteredObsMap, bool FitMaps, class Engine, class Logger>
 void find_and_fit_filtered_observation_maps_if_needed(
     Engine &engine, const Logger &logger) {
     if (should_find_filtered_observation_sources(engine)) {
-        logger->info("finding filtered obs map sources");
-        engine.template find_sources<FilteredObsMap>(engine.omb);
+        find_map_sources_with_log<FilteredObsMap>(
+            engine, engine.omb, logger, "finding filtered obs map sources");
     }
 
     fit_filtered_observation_maps_if_requested<FitMaps>(engine);
