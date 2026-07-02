bcbbba1c3613462cce48a13a4c6e35e5d2357b6d
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 11:37:15 -0400

    add raw kids interface collector

--- include/citlali/core/pipeline/array_properties_table.h
+++ include/citlali/core/pipeline/array_properties_table.h
@@ -22,6 +22,16 @@ std::vector<std::string> raw_kids_filepaths(const RawObs &rawobs) {
     return result;
 }
 
+template <class RawObs>
+std::vector<std::string> raw_kids_interfaces(const RawObs &rawobs) {
+    std::vector<std::string> result;
+    for (const auto &data_item : rawobs.kidsdata()) {
+        const auto &item = detail::unwrap_reference_wrapper(data_item);
+        result.push_back(item.interface());
+    }
+    return result;
+}
+
 template <class Engine, class RawObs, class Logger>
 void load_array_properties_table(Engine &engine, const RawObs &rawobs,
                                  const Logger &logger) {
