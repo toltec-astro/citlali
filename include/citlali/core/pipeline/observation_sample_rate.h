d3199bcd6b0254e772a704fc5e1d063c2070a4ee
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 12:15:14 -0400

    add downsample run gate

--- include/citlali/core/pipeline/observation_sample_rate.h
+++ include/citlali/core/pipeline/observation_sample_rate.h
@@ -5,6 +5,11 @@
 namespace citlali::pipeline {
 
 template <class Engine>
+bool should_run_downsample(const Engine &engine) {
+    return engine.rtcproc.run_downsample;
+}
+
+template <class Engine>
 bool downsample_factor_requires_frequency(const Engine &engine) {
     return engine.rtcproc.downsampler.factor <= 0;
 }
