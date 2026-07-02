60c67f1749d0e5164d93154ea0013c23b345f21e
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 13:29:25 -0400

    split fruit loop iteration state helpers

--- include/citlali/core/pipeline/iteration_lifecycle.h
+++ include/citlali/core/pipeline/iteration_lifecycle.h
@@ -1,31 +1,10 @@
 #pragma once
 
+#include <citlali/core/pipeline/fruit_loop_iteration_state.h>
 #include <citlali/core/pipeline/reduction_learning_lifecycle.h>
 #include <citlali/core/pipeline/weight_validation_lifecycle.h>
 
 namespace citlali::pipeline {
-
-struct ReductionIterationState {
-    bool fruit_loops_converged = false;
-};
-
-inline void reset_reduction_iteration_state(ReductionIterationState &state) {
-    state.fruit_loops_converged = false;
-}
-
-template <class Engine>
-bool fruit_loop_iteration_pending(const Engine &engine,
-                                  bool fruit_loops_converged) {
-    return (engine.fruit_iter < engine.ptcproc.fruit_loops_iters) &&
-           !fruit_loops_converged;
-}
-
-template <class Engine>
-bool fruit_loop_iteration_pending(const Engine &engine,
-                                  const ReductionIterationState &state) {
-    return fruit_loop_iteration_pending(engine,
-                                        state.fruit_loops_converged);
-}
 
 template <class Engine>
 bool should_log_fruit_loop_iteration_start(const Engine &engine) {
