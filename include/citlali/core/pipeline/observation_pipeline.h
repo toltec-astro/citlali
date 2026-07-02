9dd6804f85be785b250a0477ff17522fc5805cc8
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 09:40:23 -0400

    add observation pipeline setup helper

--- include/citlali/core/pipeline/observation_pipeline.h
+++ include/citlali/core/pipeline/observation_pipeline.h
@@ -7,6 +7,12 @@ bool should_run_observation_tod(const Engine &engine) {
     return engine.run_tod;
 }
 
+template <class Engine, class Logger>
+void setup_observation_pipeline(Engine &engine, const Logger &logger) {
+    logger->info("pipeline setup");
+    engine.setup();
+}
+
 template <class Engine, class KidsProc, class RawObs, class Logger>
 void setup_and_run_observation_pipeline(Engine &engine, KidsProc &kidsproc,
                                         const RawObs &rawobs,
