acf60498a37e412e4c641f3df63f430926864690
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 12:13:29 -0400

    use observation exposure calculation helper

--- include/citlali/core/pipeline/observation_exposure_time.h
+++ include/citlali/core/pipeline/observation_exposure_time.h
@@ -20,10 +20,7 @@ auto calculate_observation_exposure_time(const Engine &engine) {
 
 template <class Engine>
 void update_observation_exposure_time(Engine &engine) {
-    auto t0 = observation_start_time(engine);
-    auto tn = observation_stop_time(engine);
-
-    engine.omb.exposure_time = tn - t0;
+    engine.omb.exposure_time = calculate_observation_exposure_time(engine);
     if (engine.run_coadd) {
         engine.cmb.exposure_time =
             engine.cmb.exposure_time + engine.omb.exposure_time;
