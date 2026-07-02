225b0c64b4b0153f2aa469af4dd4774604cd8476
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 09:21:22 -0400

    add reduction observation date append helper

--- include/citlali/core/pipeline/observation_date.h
+++ include/citlali/core/pipeline/observation_date.h
@@ -9,6 +9,11 @@ void append_observation_date(Engine &engine, DateObs &&date_obs) {
     engine.date_obs.push_back(std::forward<DateObs>(date_obs));
 }
 
+template <class Engine, class DateObs>
+void append_reduction_observation_date(Engine &engine, DateObs &&date_obs) {
+    append_observation_date(engine, std::forward<DateObs>(date_obs));
+}
+
 template <class Engine, class ConvertUnixToUtc>
 auto date_obs_from_telescope_time(Engine &engine,
                                   ConvertUnixToUtc &&convert_unix_to_utc) {
