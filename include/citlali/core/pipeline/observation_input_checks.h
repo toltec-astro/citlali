8be8ace0c31dff00cd5fd6ba11be6ba0544b85b9
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 12:26:59 -0400

    add observation input check helper

--- include/citlali/core/pipeline/observation_input_checks.h
+++ include/citlali/core/pipeline/observation_input_checks.h
@@ -7,6 +7,11 @@ void log_observation_input_check(const Logger &logger) {
     logger->debug("checking inputs");
 }
 
+template <class TodProc, class RawObs>
+void run_observation_input_check(TodProc &todproc, const RawObs &rawobs) {
+    todproc.check_inputs(rawobs);
+}
+
 template <class TodProc, class RawObs, class Logger>
 void check_observation_inputs(TodProc &todproc, const RawObs &rawobs,
                               const Logger &logger) {
