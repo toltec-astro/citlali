1a9fa68af74782c9cfe2a98816bfb26fbaf42ad6
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-02 11:05:21 -0400

    add hwpr file load helper

--- include/citlali/core/pipeline/hwpr_loading.h
+++ include/citlali/core/pipeline/hwpr_loading.h
@@ -45,6 +45,13 @@ void log_hwpr_ignored_if_needed(const Engine &engine, const Logger &logger) {
     }
 }
 
+template <class Engine, class Logger>
+void load_hwpr_file(Engine &engine, const std::string &filepath,
+                    const Logger &logger) {
+    logger->info("getting hwpr file {}", filepath);
+    engine.calib.get_hwpr(filepath, engine.telescope.sim_obs);
+}
+
 template <class Engine, class RawObs, class Logger>
 void load_hwpr_data_if_requested(Engine &engine, const RawObs &rawobs,
                                  const Logger &logger) {
