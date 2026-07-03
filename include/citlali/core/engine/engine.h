6f4dd3877c34deaae94a9e8d18b26d2b6977d1dc
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-03 19:08:17 -0400

    extract rtc tod stream diagnostics

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -5681,103 +5681,12 @@ void Engine::create_tod_files() {
     if constexpr (prod_t == engine_utils::toltecIO::rtc_timestream) {
         const int fill_int = citlali::pipeline::rtcdiag_fill_int();
         const double fill_double = citlali::pipeline::rtcdiag_fill_double();
-        std::vector<netCDF::NcDim> rtc_det_dims = {n_scans_dim, n_dets_dim};
-        const std::vector<std::size_t> rtc_stream_no_chunks;
-        const auto n_rtc_stream_det_values =
-            static_cast<std::size_t>(n_tod_output_scans_for_stream) *
-            static_cast<std::size_t>(calib.n_dets);
-
-        auto add_rtc_det_double = [&](const std::string &name, const std::string &comment) {
-            citlali::pipeline::add_rtcdiag_det_double(
-                fo, name, comment, rtc_det_dims, rtc_stream_no_chunks,
-                n_rtc_stream_det_values, fill_double);
-        };
-        auto add_rtc_det_int = [&](const std::string &name, const std::string &comment) {
-            citlali::pipeline::add_rtcdiag_det_int(
-                fo, name, comment, rtc_det_dims, rtc_stream_no_chunks,
-                n_rtc_stream_det_values, fill_int);
-        };
-
-        citlali::pipeline::add_rtcdiag_detector_core_diag(
-            add_rtc_det_int, add_rtc_det_double);
-
-        netCDF::NcDim n_nws_rtcdiag_dim = fo.addDim("n_nws_rtcdiag", calib.n_nws);
-        citlali::pipeline::add_rtcdiag_network_ids(
-            fo, calib, n_nws_rtcdiag_dim, fill_int);
-
-        std::vector<netCDF::NcDim> rtc_nw_dims = {n_scans_dim, n_nws_rtcdiag_dim};
-        const auto n_rtc_stream_nw_values =
-            static_cast<std::size_t>(n_tod_output_scans_for_stream) *
-            static_cast<std::size_t>(calib.n_nws);
-        auto add_rtc_nw_double = [&](const std::string &name, const std::string &comment) {
-            citlali::pipeline::add_rtcdiag_network_double(
-                fo, name, comment, rtc_nw_dims, rtc_stream_no_chunks,
-                n_rtc_stream_nw_values, fill_double);
-        };
-        auto add_rtc_nw_int = [&](const std::string &name, const std::string &comment) {
-            citlali::pipeline::add_rtcdiag_network_int(
-                fo, name, comment, rtc_nw_dims, rtc_stream_no_chunks,
-                n_rtc_stream_nw_values, fill_int);
-        };
-
-        citlali::pipeline::add_rtcdiag_standard_network_diag(
-            add_rtc_nw_int, add_rtc_nw_double);
-
-        if (rtcproc.impulsive_capture.enabled) {
-            const auto n_slots = static_cast<std::size_t>(std::max<Eigen::Index>(rtcproc.impulsive_capture.max_events_per_network, 1));
-            const double rtc_fsmp = rtcproc.run_downsample ? telescope.d_fsmp : telescope.fsmp;
-            const auto snippet_pre = static_cast<std::size_t>(std::max(0.0, std::round(rtcproc.impulsive_capture.snippet_pre_window_sec * rtc_fsmp)));
-            const auto snippet_post = static_cast<std::size_t>(std::max(0.0, std::round(rtcproc.impulsive_capture.snippet_post_window_sec * rtc_fsmp)));
-            const auto n_snippet = snippet_pre + snippet_post + 1;
-            netCDF::NcDim n_rtc_impulsive_slots_dim = fo.addDim("n_rtc_impulsive_slots", n_slots);
-            netCDF::NcDim n_rtc_impulsive_samples_dim = fo.addDim("n_rtc_impulsive_samples", n_snippet);
-
-            netCDF::NcVar offset_v = fo.addVar("rtc_impulsive_snippet_offset_samples", netCDF::ncInt, n_rtc_impulsive_samples_dim);
-            offset_v.putAtt("units", "samples");
-            offset_v.putAtt("comment", "sample offsets relative to rtc_impulsive_slot_event_sample");
-            const auto offsets =
-                citlali::pipeline::rtcdiag_impulsive_snippet_offsets(
-                    n_snippet, snippet_pre, fill_int);
-            offset_v.putVar(offsets.data());
-
-            std::vector<netCDF::NcDim> rtc_impulsive_slot_dims = {n_scans_dim, n_nws_rtcdiag_dim, n_rtc_impulsive_slots_dim};
-            std::vector<netCDF::NcDim> rtc_impulsive_snippet_dims = {n_scans_dim, n_nws_rtcdiag_dim, n_rtc_impulsive_slots_dim, n_rtc_impulsive_samples_dim};
-            const auto n_rtc_stream_impulsive_slot_values =
-                static_cast<std::size_t>(n_tod_output_scans_for_stream) *
-                static_cast<std::size_t>(calib.n_nws) * n_slots;
-            const auto n_rtc_stream_impulsive_snippet_values =
-                n_rtc_stream_impulsive_slot_values * n_snippet;
-
-            auto add_rtc_imp_slot_double = [&](const std::string &name, const std::string &comment) {
-                citlali::pipeline::add_rtcdiag_impulsive_slot_double(
-                    fo, name, comment, rtc_impulsive_slot_dims,
-                    rtc_stream_no_chunks, n_rtc_stream_impulsive_slot_values,
-                    fill_double);
-            };
-            auto add_rtc_imp_slot_int = [&](const std::string &name, const std::string &comment) {
-                citlali::pipeline::add_rtcdiag_impulsive_slot_int(
-                    fo, name, comment, rtc_impulsive_slot_dims,
-                    rtc_stream_no_chunks, n_rtc_stream_impulsive_slot_values,
-                    fill_int);
-            };
-            auto add_rtc_imp_snip_double = [&](const std::string &name, const std::string &comment) {
-                citlali::pipeline::add_rtcdiag_impulsive_snippet_double(
-                    fo, name, comment, rtc_impulsive_snippet_dims,
-                    rtc_stream_no_chunks, n_rtc_stream_impulsive_snippet_values,
-                    fill_double);
-            };
-            auto add_rtc_imp_snip_int = [&](const std::string &name, const std::string &comment) {
-                citlali::pipeline::add_rtcdiag_impulsive_snippet_int(
-                    fo, name, comment, rtc_impulsive_snippet_dims,
-                    rtc_stream_no_chunks, n_rtc_stream_impulsive_snippet_values,
-                    fill_int);
-            };
-
-            citlali::pipeline::add_rtcdiag_impulsive_capture_diag(
-                add_rtc_imp_slot_int, add_rtc_imp_slot_double,
-                add_rtc_imp_snip_double, add_rtc_imp_snip_int,
-                citlali::pipeline::rtcdiag_impulsive_capture_stream_comments());
-        }
+        const double rtc_stream_fsmp =
+            rtcproc.run_downsample ? telescope.d_fsmp : telescope.fsmp;
+        citlali::pipeline::add_rtcdiag_tod_stream_diag(
+            fo, calib, rtcproc, n_scans_dim, n_dets_dim,
+            n_tod_output_scans_for_stream, rtc_stream_fsmp,
+            fill_int, fill_double);
     }
 
     // add weights
--- include/citlali/core/pipeline/rtcdiag_netcdf.h
+++ include/citlali/core/pipeline/rtcdiag_netcdf.h
@@ -1006,4 +1006,116 @@ void add_rtcdiag_impulsive_capture_diag(
                     comments.snippet_flag);
 }
 
+template <class Calib, class Rtcproc>
+void add_rtcdiag_tod_stream_diag(netCDF::NcFile &fo, const Calib &calib,
+                                 const Rtcproc &rtcproc,
+                                 netCDF::NcDim n_scans_dim,
+                                 netCDF::NcDim n_dets_dim,
+                                 Eigen::Index n_scans,
+                                 double sample_rate_hz,
+                                 int fill_int,
+                                 double fill_double) {
+    const std::vector<std::size_t> no_chunks;
+    std::vector<netCDF::NcDim> rtc_det_dims = {n_scans_dim, n_dets_dim};
+    const auto n_det_values =
+        static_cast<std::size_t>(n_scans) *
+        static_cast<std::size_t>(calib.n_dets);
+
+    auto add_det_double = [&](const std::string &name, const std::string &comment) {
+        add_rtcdiag_det_double(
+            fo, name, comment, rtc_det_dims, no_chunks,
+            n_det_values, fill_double);
+    };
+    auto add_det_int = [&](const std::string &name, const std::string &comment) {
+        add_rtcdiag_det_int(
+            fo, name, comment, rtc_det_dims, no_chunks,
+            n_det_values, fill_int);
+    };
+
+    add_rtcdiag_detector_core_diag(add_det_int, add_det_double);
+
+    netCDF::NcDim n_nws_rtcdiag_dim =
+        fo.addDim("n_nws_rtcdiag", calib.n_nws);
+    add_rtcdiag_network_ids(fo, calib, n_nws_rtcdiag_dim, fill_int);
+
+    std::vector<netCDF::NcDim> rtc_nw_dims = {
+        n_scans_dim, n_nws_rtcdiag_dim};
+    const auto n_nw_values =
+        static_cast<std::size_t>(n_scans) *
+        static_cast<std::size_t>(calib.n_nws);
+    auto add_nw_double = [&](const std::string &name, const std::string &comment) {
+        add_rtcdiag_network_double(
+            fo, name, comment, rtc_nw_dims, no_chunks,
+            n_nw_values, fill_double);
+    };
+    auto add_nw_int = [&](const std::string &name, const std::string &comment) {
+        add_rtcdiag_network_int(
+            fo, name, comment, rtc_nw_dims, no_chunks,
+            n_nw_values, fill_int);
+    };
+
+    add_rtcdiag_standard_network_diag(add_nw_int, add_nw_double);
+
+    if (!rtcproc.impulsive_capture.enabled) {
+        return;
+    }
+
+    const auto n_slots = static_cast<std::size_t>(
+        std::max<Eigen::Index>(rtcproc.impulsive_capture.max_events_per_network, 1));
+    const auto snippet_pre = static_cast<std::size_t>(std::max(
+        0.0, std::round(rtcproc.impulsive_capture.snippet_pre_window_sec *
+                        sample_rate_hz)));
+    const auto snippet_post = static_cast<std::size_t>(std::max(
+        0.0, std::round(rtcproc.impulsive_capture.snippet_post_window_sec *
+                        sample_rate_hz)));
+    const auto n_snippet = snippet_pre + snippet_post + 1;
+    netCDF::NcDim n_rtc_impulsive_slots_dim =
+        fo.addDim("n_rtc_impulsive_slots", n_slots);
+    netCDF::NcDim n_rtc_impulsive_samples_dim =
+        fo.addDim("n_rtc_impulsive_samples", n_snippet);
+
+    netCDF::NcVar offset_v =
+        fo.addVar("rtc_impulsive_snippet_offset_samples", netCDF::ncInt,
+                  n_rtc_impulsive_samples_dim);
+    offset_v.putAtt("units", "samples");
+    offset_v.putAtt(
+        "comment", "sample offsets relative to rtc_impulsive_slot_event_sample");
+    const auto offsets =
+        rtcdiag_impulsive_snippet_offsets(n_snippet, snippet_pre, fill_int);
+    offset_v.putVar(offsets.data());
+
+    std::vector<netCDF::NcDim> slot_dims = {
+        n_scans_dim, n_nws_rtcdiag_dim, n_rtc_impulsive_slots_dim};
+    std::vector<netCDF::NcDim> snippet_dims = {
+        n_scans_dim, n_nws_rtcdiag_dim, n_rtc_impulsive_slots_dim,
+        n_rtc_impulsive_samples_dim};
+    const auto n_slot_values = n_nw_values * n_slots;
+    const auto n_snippet_values = n_slot_values * n_snippet;
+
+    auto add_slot_double = [&](const std::string &name, const std::string &comment) {
+        add_rtcdiag_impulsive_slot_double(
+            fo, name, comment, slot_dims, no_chunks,
+            n_slot_values, fill_double);
+    };
+    auto add_slot_int = [&](const std::string &name, const std::string &comment) {
+        add_rtcdiag_impulsive_slot_int(
+            fo, name, comment, slot_dims, no_chunks,
+            n_slot_values, fill_int);
+    };
+    auto add_snippet_double = [&](const std::string &name, const std::string &comment) {
+        add_rtcdiag_impulsive_snippet_double(
+            fo, name, comment, snippet_dims, no_chunks,
+            n_snippet_values, fill_double);
+    };
+    auto add_snippet_int = [&](const std::string &name, const std::string &comment) {
+        add_rtcdiag_impulsive_snippet_int(
+            fo, name, comment, snippet_dims, no_chunks,
+            n_snippet_values, fill_int);
+    };
+
+    add_rtcdiag_impulsive_capture_diag(
+        add_slot_int, add_slot_double, add_snippet_double, add_snippet_int,
+        rtcdiag_impulsive_capture_stream_comments());
+}
+
 }  // namespace citlali::pipeline
