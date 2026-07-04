3dafa6b6cbfba02eb5738e9aff658f284b52bc08
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 08:22:01 -0400

    extract mapdiag observation total pair

--- include/citlali/core/pipeline/mapdiag_observation_weight.h
+++ include/citlali/core/pipeline/mapdiag_observation_weight.h
@@ -120,6 +120,14 @@ double sum_mapdiag_obs_values(const Values &values, std::size_t n_obsnums,
     return total;
 }
 
+template <class Values>
+MapdiagObsWeightTotals sum_mapdiag_obs_weight_totals(
+    const Values &weight_sum, const Values &core_weight_sum,
+    std::size_t n_obsnums, std::size_t map_index) {
+    return {sum_mapdiag_obs_values(weight_sum, n_obsnums, map_index),
+            sum_mapdiag_obs_values(core_weight_sum, n_obsnums, map_index)};
+}
+
 template <class DoubleValues, class IntValues>
 void assign_mapdiag_single_obs_entry(
     std::size_t flat, double map_weight_sum, double map_core_weight_sum,
