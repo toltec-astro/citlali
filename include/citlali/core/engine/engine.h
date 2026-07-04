bc173897fb2b6537ef042a11fa76fba555bb2168
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 18:57:32 -0400

    inline mapdiag total weight access

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -6633,11 +6633,6 @@ void Engine::write_mapdiag(map_buffer_t &mb, std::string dir_name) {
             weight_thresholds[idx] = weight_threshold;
             return weight_threshold;
         };
-    auto mapdiag_current_total_weight =
-        [&](std::size_t map_st, Eigen::Index row, Eigen::Index col) {
-            return citlali::pipeline::mapdiag_matrix_double_value(
-                mb->contribution_total_weight[map_st], row, col);
-        };
     auto mapdiag_current_total_variance_weight =
         [&](std::size_t map_st, Eigen::Index row, Eigen::Index col) {
             return citlali::pipeline::mapdiag_matrix_double_value(
@@ -6874,8 +6869,10 @@ void Engine::write_mapdiag(map_buffer_t &mb, std::string dir_name) {
                                                     mb->contribution_total_signal[map_st],
                                                     r, c);
                                         const double total_weight =
-                                            mapdiag_current_total_weight(
-                                                map_st, r, c);
+                                            citlali::pipeline::
+                                                mapdiag_matrix_double_value(
+                                                    mb->contribution_total_weight[map_st],
+                                                    r, c);
                                         const double total_variance_weight =
                                             mapdiag_current_total_variance_weight(
                                                 map_st, r, c);
