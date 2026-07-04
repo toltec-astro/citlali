d0718523c7850d539f1061483d710d45d36697b6
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 08:29:46 -0400

    add mapdiag edge guard double refs

--- include/citlali/core/pipeline/mapdiag_edge_guard.h
+++ include/citlali/core/pipeline/mapdiag_edge_guard.h
@@ -12,4 +12,18 @@ struct MapdiagEdgeGuardIntRefs {
     std::vector<int> &guardband_npix;
 };
 
+struct MapdiagEdgeGuardDoubleRefs {
+    std::vector<double> &weight_thresholds;
+    std::vector<double> &hits_thresholds;
+    std::vector<double> &background_levels;
+    std::vector<double> &science_frac;
+    std::vector<double> &support_frac;
+    std::vector<double> &guardband_rms_pre;
+    std::vector<double> &guardband_rms_post;
+    std::vector<double> &exterior_rms_pre;
+    std::vector<double> &exterior_rms_post;
+    std::vector<double> &exterior_max_abs_pre;
+    std::vector<double> &exterior_max_abs_post;
+};
+
 }  // namespace citlali::pipeline
