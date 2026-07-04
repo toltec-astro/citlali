163bc7bcc3e85a078e15ae37429d2aa65223edcd
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 14:46:00 -0400

    add mapdiag contribution usage helper

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -6775,6 +6775,10 @@ void Engine::write_mapdiag(map_buffer_t &mb, std::string dir_name) {
             return citlali::pipeline::mapdiag_has_contribution_products(
                 mb, map_i);
         };
+    auto mapdiag_current_uses_contribution_products =
+        [&](Eigen::Index map_i) {
+            return mapdiag_current_has_contribution_products(map_i);
+        };
     auto mapdiag_current_ptc_fs_hz =
         [&]() {
             return processed_time_chunk_fs_hz();
