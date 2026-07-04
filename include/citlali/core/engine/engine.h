8df593b68806e78b73fe90833c6bc3f299e98b37
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-03 20:34:56 -0400

    reuse signal map description helper

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -5988,7 +5988,7 @@ void Engine::write_maps(fits_io_type &fits_io, fits_io_type &noise_fits_io, map_
             mb->signal[i]);
         citlali::pipeline::add_image_unit_description_keys(
             *fits_io->at(map_index).hdus.back(), mb->sig_unit,
-            "Signal map in map units");
+            citlali::pipeline::signal_map_description());
 
         // weight map
         add_map_hdu_with_wcs(
