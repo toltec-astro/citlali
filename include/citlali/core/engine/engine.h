12118a1e743ebc0c2fb7c5537687e7414631998f
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 13:06:55 -0400

    add mapdiag total variance accessor

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -6831,6 +6831,10 @@ void Engine::write_mapdiag(map_buffer_t &mb, std::string dir_name) {
         [&](std::size_t map_st, Eigen::Index row, Eigen::Index col) {
             return mb->contribution_total_weight[map_st](row, col);
         };
+    auto mapdiag_current_total_variance_weight =
+        [&](std::size_t map_st, Eigen::Index row, Eigen::Index col) {
+            return mb->contribution_total_variance_weight[map_st](row, col);
+        };
 
     for (Eigen::Index i = 0; i < n_maps; ++i) {
         const std::size_t idx = static_cast<std::size_t>(i);
