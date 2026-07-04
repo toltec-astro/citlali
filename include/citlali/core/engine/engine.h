6390e33b8c79ea84f1ea80c3a882714c7d22f6dc
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 05:34:57 -0400

    reuse mapdiag observation double var helper

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -6950,8 +6950,8 @@ void Engine::write_mapdiag(map_buffer_t &mb, std::string dir_name) {
             fo, mapdiag_dims, name, comment, values);
     };
     auto add_map_obs_double = [&](const std::string &name, const std::string &comment, const std::vector<double> &values) {
-        citlali::pipeline::add_mapdiag_double_2d(
-            fo, name, comment, mapdiag_dims.map_obs, values);
+        citlali::pipeline::add_mapdiag_obs_double_var(
+            fo, mapdiag_dims, name, comment, values);
     };
     auto add_map_obs_int = [&](const std::string &name, const std::string &comment, const std::vector<int> &values) {
         citlali::pipeline::add_mapdiag_int_2d(
