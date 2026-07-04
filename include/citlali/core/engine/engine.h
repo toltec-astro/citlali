e0c1cd2fd7fe3103968d932a9f3b22db3ff7957a
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 13:46:57 -0400

    reuse mapdiag leave one out z helper

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -6995,11 +6995,9 @@ void Engine::write_mapdiag(map_buffer_t &mb, std::string dir_name) {
                                                     total_signal,
                                                     contrib_signal,
                                                     remaining_weight);
-                                            citlali::pipeline::
-                                                mapdiag_assign_leave_one_out_z(
-                                                    value, wt, loo_value,
-                                                    candidate
-                                                        .leave_one_out_z);
+                                            assign_mapdiag_current_leave_one_out_z(
+                                                value, wt, loo_value,
+                                                candidate);
                                         }
                                         else if (citlali::pipeline::
                                                      mapdiag_has_fallback_leave_one_out_inputs(
