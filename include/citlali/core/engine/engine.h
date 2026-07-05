389dd6be2553ff3b5878cb96dd90fa6bf4a246b7
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 21:35:35 -0400

    format wiener filter map call

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -7538,7 +7538,7 @@ void Engine::run_wiener_filter(map_buffer_t &mb) {
         // run the filter for the current map
         logger->info("running Wiener filter core for {} map {}/{} (array={})",
                      map_label, i + 1, n_maps, array_name);
-        wiener_filter.filter_maps(mb,i);
+        wiener_filter.filter_maps(mb, i);
         logger->info("map filtering complete for {} map {}/{}", map_label, i + 1, n_maps);
 
         // filter noise maps
