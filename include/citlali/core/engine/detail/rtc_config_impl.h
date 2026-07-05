69ef1f68c222bab3af88129f4c1dd33ea277729c
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-05 15:07:02 -0400

    Extract raw TOD context policy

--- include/citlali/core/engine/detail/rtc_config_impl.h
+++ include/citlali/core/engine/detail/rtc_config_impl.h
@@ -132,20 +132,7 @@ void Engine::get_rtc_config(CT &config) {
     rtcproc.configure_filter_edge_guard(telescope.fsmp);
     citlali::pipeline::mirror_raw_filter_edge_guard_config(
         typed_filter.edge_guard, rtcproc.filter_edge_guard);
-    telescope.inner_scans_chunk = rtcproc.filter_edge_guard.context_samples;
-    telescope.outer_scans_chunk = telescope.inner_scans_chunk;
-    if (rtcproc.tod_output_outer) {
-        telescope.outer_scans_chunk = std::max<Eigen::Index>(
-            telescope.outer_scans_chunk,
-            std::max<Eigen::Index>(0, rtcproc.tod_output_outer_context_samples));
-    }
-    if (rtcproc.line_audit.enabled &&
-        rtcproc.line_audit.post_filter_enabled &&
-        rtcproc.line_audit.post_filter_apply_detector_notches) {
-        telescope.outer_scans_chunk = std::max<Eigen::Index>(
-            telescope.outer_scans_chunk,
-            std::max<Eigen::Index>(0, rtcproc.line_audit.detector_notch_context_samples));
-    }
+    citlali::pipeline::configure_raw_tod_output_context(telescope, rtcproc);
 
     // ignore hwpr?
     get_config_value(config, calib.ignore_hwpr, missing_keys, invalid_keys,
--- include/citlali/core/pipeline/timestream_config_mirror.h
+++ include/citlali/core/pipeline/timestream_config_mirror.h
@@ -2,6 +2,7 @@
 
 #include <citlali/core/config/timestream_config.h>
 
+#include <algorithm>
 #include <cstddef>
 #include <vector>
 
@@ -538,6 +539,26 @@ void mirror_raw_filter_edge_guard_config(EdgeGuardConfig &target,
     target.apply_dynamic_notch = source.apply_dynamic_notch;
     target.apply_iir_highpass = source.apply_iir_highpass;
     target.apply_downsample = source.apply_downsample;
+}
+
+template <class Telescope, class RtcProc>
+void configure_raw_tod_output_context(Telescope &telescope,
+                                      const RtcProc &rtcproc) {
+    telescope.inner_scans_chunk = rtcproc.filter_edge_guard.context_samples;
+    telescope.outer_scans_chunk = telescope.inner_scans_chunk;
+    if (rtcproc.tod_output_outer) {
+        telescope.outer_scans_chunk = std::max<Eigen::Index>(
+            telescope.outer_scans_chunk,
+            std::max<Eigen::Index>(
+                0, rtcproc.tod_output_outer_context_samples));
+    }
+    if (rtcproc.line_audit.enabled && rtcproc.line_audit.post_filter_enabled &&
+        rtcproc.line_audit.post_filter_apply_detector_notches) {
+        telescope.outer_scans_chunk = std::max<Eigen::Index>(
+            telescope.outer_scans_chunk,
+            std::max<Eigen::Index>(
+                0, rtcproc.line_audit.detector_notch_context_samples));
+    }
 }
 
 }  // namespace citlali::pipeline
