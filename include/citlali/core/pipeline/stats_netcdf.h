27c4386ddfe773f57128cdc563b0fedb68dc58ec
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-05 06:38:00 -0400

    add detector stats units helper

--- include/citlali/core/pipeline/stats_netcdf.h
+++ include/citlali/core/pipeline/stats_netcdf.h
@@ -12,4 +12,14 @@ inline std::string stats_unit_or_empty(
     return it == units.end() ? "" : it->second;
 }
 
+inline std::map<std::string, std::string>
+detector_stats_units(const std::string &signal_unit) {
+    return {
+        {"rms", signal_unit},
+        {"stddev", signal_unit},
+        {"median", signal_unit},
+        {"flagged_frac", "N/A"},
+        {"weights", "1/(" + signal_unit + ")^2"}};
+}
+
 }  // namespace citlali::pipeline
