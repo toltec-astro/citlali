cac2fcf2a9657f586154fe155192321fd435ae8e
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-05 07:44:53 -0400

    add map filter fwhm template predicate

--- include/citlali/core/pipeline/map_filtering.h
+++ include/citlali/core/pipeline/map_filtering.h
@@ -4,6 +4,11 @@
 
 namespace citlali::pipeline {
 
+inline bool map_filter_template_uses_fwhm(
+    const std::string &template_type) {
+    return template_type == "gaussian" || template_type == "airy";
+}
+
 template <auto FilteredMap, class Engine, class MapBuffer, class Logger>
 void run_wiener_filter_with_log(Engine &engine, MapBuffer &map_buffer,
                                 const Logger &logger,
