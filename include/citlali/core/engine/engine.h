7913c9281e930775122c6dd1b4af73068b27e79a
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 22:03:00 -0400

    const qualify psd product locals

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -6236,12 +6236,12 @@ void Engine::write_psd(map_buffer_t &mb, std::string dir_name) {
     // loop through psd vector
     for (Eigen::Index i = 0; i < mb->psds.size(); ++i) {
         // get name for extension layer
-        std::string map_name = get_map_name(i);
+        const std::string map_name = get_map_name(i);
 
         // get the array for the given map
-        Eigen::Index map_index = arrays_to_maps(i);
+        const Eigen::Index map_index = arrays_to_maps(i);
         // get the stokes parameter for the given map
-        Eigen::Index stokes_index = maps_to_stokes(i);
+        const Eigen::Index stokes_index = maps_to_stokes(i);
 
         const std::string name = citlali::pipeline::spectral_product_name(
             toltec_io.array_name_map, calib.arrays,
