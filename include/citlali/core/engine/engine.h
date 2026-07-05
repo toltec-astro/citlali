a176804e9dd94b9812698ef7ac91d8ec8d15f97f
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-04 22:04:23 -0400

    trim histogram product comments

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -6266,13 +6266,9 @@ void Engine::write_hist(map_buffer_t &mb, std::string dir_name) {
 
     // loop through stored histograms
     for (Eigen::Index i = 0; i < mb->hists.size(); ++i) {
-        // string to hold name
-        // get name for extension layer
         const std::string map_name = get_map_name(i);
 
-        // get the array for the given map
         const Eigen::Index map_index = arrays_to_maps(i);
-        // get the stokes parameter for the given map
         const Eigen::Index stokes_index = maps_to_stokes(i);
 
         const std::string name = citlali::pipeline::spectral_product_name(
