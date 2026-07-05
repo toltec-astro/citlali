d079135c6fc87f80ab0530b21667739fe294b15e
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-05 06:30:26 -0400

    use rtcdiag helper for impulsive snippet size

--- include/citlali/core/engine/engine.h
+++ include/citlali/core/engine/engine.h
@@ -7365,7 +7365,9 @@ void Engine::create_rtcdiag_file() {
         const auto snippet_post =
             citlali::pipeline::rtcdiag_impulsive_window_samples(
                 snippet_post_window_sec, rtc_fsmp);
-        const auto n_snippet = snippet_pre + snippet_post + 1;
+        const auto n_snippet =
+            citlali::pipeline::rtcdiag_impulsive_snippet_sample_count(
+                snippet_pre, snippet_post);
         netCDF::NcDim n_rtc_impulsive_slots_dim =
             fo.addDim("n_rtc_impulsive_slots", n_slots);
         netCDF::NcDim n_rtc_impulsive_samples_dim =
