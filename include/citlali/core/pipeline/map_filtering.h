fbf8b9d12c549afdac48a0143dd6c428e12a6ce8
GrantWWilson <44026302+GrantWWilson@users.noreply.github.com>
2026-07-05 07:46:02 -0400

    add map filter initial fwhm helper

--- include/citlali/core/pipeline/map_filtering.h
+++ include/citlali/core/pipeline/map_filtering.h
@@ -9,6 +9,11 @@ inline bool map_filter_template_uses_fwhm(
     return template_type == "gaussian" || template_type == "airy";
 }
 
+inline double map_filter_initial_fwhm_pixels(
+    double array_fwhm_arcsec, double arcsec_to_rad, double pixel_size_rad) {
+    return array_fwhm_arcsec * arcsec_to_rad / pixel_size_rad;
+}
+
 template <auto FilteredMap, class Engine, class MapBuffer, class Logger>
 void run_wiener_filter_with_log(Engine &engine, MapBuffer &map_buffer,
                                 const Logger &logger,
